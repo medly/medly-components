21eebfc38d5848fa7d02df8184fd742fb8fef36f
Mukul Bansal <mukulbansal01@gmail.com>
2019-07-19 14:34:32 +0530

    fix: added file format of the fonts
    affects: @medly-components/theme

--- packages/theme/src/core/font/faces.ts
+++ packages/theme/src/core/font/faces.ts
@@ -5,19 +5,19 @@ const proximaNova: FontFaceTheme = {
     fontFamily: 'ProximaNova',
     fontFaces: [
         {
-            primarySrc: `url('https://d1b6bucc9jhzue.cloudfront.net/at/fonts/ProximaNova/webfonts/ProximaNovaLight.eot?cloudFrontFixScheme=http&cloudFrontFixHost=www2.vauto.com')`,
+            primarySrc: `url('https://d1b6bucc9jhzue.cloudfront.net/at/fonts/ProximaNova/webfonts/ProximaNovaLight.eot?cloudFrontFixScheme=http&cloudFrontFixHost=www2.vauto.com') format('eot')`,
             secondarySrc: '',
             fontWeight: 200,
             fontStyle: 'normal'
         },
         {
-            primarySrc: `url('https://d1b6bucc9jhzue.cloudfront.net/at/fonts/ProximaNova/webfonts/ProximaNovaRegular.eot?cloudFrontFixScheme=http&cloudFrontFixHost=www2.vauto.com')`,
+            primarySrc: `url('https://d1b6bucc9jhzue.cloudfront.net/at/fonts/ProximaNova/webfonts/ProximaNovaRegular.eot?cloudFrontFixScheme=http&cloudFrontFixHost=www2.vauto.com') format('eot')`,
             secondarySrc: '',
             fontWeight: 400,
             fontStyle: 'normal'
         },
         {
-            primarySrc: `url('https://d1b6bucc9jhzue.cloudfront.net/at/fonts/ProximaNova/webfonts/ProximaNovaBold.eot?cloudFrontFixScheme=http&cloudFrontFixHost=www2.vauto.com')`,
+            primarySrc: `url('https://d1b6bucc9jhzue.cloudfront.net/at/fonts/ProximaNova/webfonts/ProximaNovaBold.eot?cloudFrontFixScheme=http&cloudFrontFixHost=www2.vauto.com') format('eot')`,
             secondarySrc: '',
             fontWeight: 700,
             fontStyle: 'normal'
