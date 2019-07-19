261360434dcd02de4cde438350d00696323d16d5
Mukul Bansal <mukulbansal01@gmail.com>
2019-07-19 12:49:25 +0530

    feat: added capitalize function to capitalize first letter of the string
    affects: @medly-components/utils

--- packages/utils/src/helpers/stringHelpers.ts
+++ packages/utils/src/helpers/stringHelpers.ts
@@ -1,2 +1,3 @@
 export const equalsIgnoreCase = (str1: string, str2: string) => str1.toLowerCase() === str2.toLowerCase();
 export const includesIgnoreCase = (str1: string, str2: string) => str1.toLowerCase().includes(str2.toLowerCase());
+export const capitalize = (s: string) => s[0].toUpperCase() + s.substr(1);
