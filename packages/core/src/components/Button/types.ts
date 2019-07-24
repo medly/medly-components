63694313b61f6241564383828af81996381ff098
Mukul Bansal <mukulbansal01@gmail.com>
2019-07-24 13:25:17 +0530

    fix: added as prop in Button component
    affects: @medly-components/core

--- packages/core/src/components/Button/types.ts
+++ packages/core/src/components/Button/types.ts
@@ -1,6 +1,7 @@
 import { HTMLProps, WithThemeProp } from '@medly-components/utils';
 
 export interface Props extends HTMLProps<HTMLButtonElement>, WithThemeProp {
+    as?: any;
     /** Use this if rendering it like Link */
     to?: string;
     type?: 'button' | 'reset' | 'submit';
