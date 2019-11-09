92b100d98624108070d4e6c3778a13698a6ddcdb
gmukul01 <mukulbansal01@gmail.com>
2019-11-09 16:31:07 +0530

    docs(core): update chip and button component
    affects: @medly-components/core

--- packages/core/src/components/Button/Button.stories.tsx
+++ packages/core/src/components/Button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import { ButtonTheme } from '@medly-components/theme';
+import { action } from '@storybook/addon-actions';
 import { boolean, select } from '@storybook/addon-knobs';
 import React from 'react';
 import { Button } from './Button';
@@ -9,13 +10,10 @@ const color: Array<Props['color']> = ['primary', 'secondary'];
 
 export const ThemeInterface = (props: ButtonTheme): any => null;
 
-// tslint:disable-next-line: no-console
-const handleClick = () => console.log('Button Clicked');
-
 export const Basic = () => (
     <Button
         variant={select('Variant', variant, 'solid')}
-        onClick={handleClick}
+        onClick={action('Chip Clicked')}
         disabled={boolean('Disabled', false)}
         color={select('Color', color, 'primary')}
     >
--- packages/core/src/components/Chip/Chip.stories.tsx
+++ packages/core/src/components/Chip/Chip.stories.tsx
@@ -1,14 +1,15 @@
-import { defaultTheme } from '@medly-components/theme';
+import { ChipTheme, defaultTheme } from '@medly-components/theme';
 import { action } from '@storybook/addon-actions';
 import { boolean, color, select, text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { Chip } from './Chip';
 import { Props } from './types';
 
 const variant: Array<Props['variant']> = ['solid', 'flat', 'outlined'];
 
-storiesOf('Core', module).add('Chip', () => (
+export const ThemeInterface = (props: ChipTheme): any => null;
+
+export const Basic = () => (
     <Chip
         label={text('Label', 'Demo')}
         variant={select('Variant', variant, 'solid')}
@@ -17,4 +18,4 @@ storiesOf('Core', module).add('Chip', () => (
         disabled={boolean('Disabled', false)}
         color={color('Color', defaultTheme.chip.defaultColor)}
     />
-));
+);
