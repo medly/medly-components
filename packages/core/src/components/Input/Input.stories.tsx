b37eccebf6930ea1ce343ca47f7694bd7e57f0b5
gmukul01 <mukulbansal01@gmail.com>
2019-11-11 21:16:22 +0530

    docs(core): update docs of input component
    affects: @medly-components/core

--- packages/core/src/components/Input/Input.stories.tsx
+++ packages/core/src/components/Input/Input.stories.tsx
@@ -1,12 +1,14 @@
+import { InputTheme } from '@medly-components/theme';
 import { boolean, select, text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { Input } from './Input';
 import { Props } from './types';
 
 const labelPosition: Array<Props['labelPosition']> = ['top', 'bottom', 'left', 'right'];
 
-storiesOf('Core', module).add('Input', () => (
+export const ThemeInterface = (props: InputTheme): any => null;
+
+export const Basic = () => (
     <Input
         id="storybook-input"
         type="email"
@@ -18,4 +20,4 @@ storiesOf('Core', module).add('Input', () => (
         placeholder={text('Placeholder', 'Enter Email Address')}
         description={text('Description', 'We will never share your email with anyone')}
     />
-));
+);
