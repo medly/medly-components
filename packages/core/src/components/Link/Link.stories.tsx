59b039a249fa84a89a9d718b8ae65f342f967f1e
gmukul01 <mukulbansal01@gmail.com>
2019-11-11 15:57:30 +0530

    docs(core): update docs for link components
    affects: @medly-components/core

---
+++ packages/core/src/components/Link/Link.stories.tsx
@@ -0,0 +1,7 @@
+import { LinkTheme } from '@medly-components/theme';
+import React from 'react';
+import { Link } from './Link';
+
+export const ThemeInterface = (props: LinkTheme): any => null;
+
+export const Basic = () => <Link href="#">Add New Pharmacy</Link>;
--- packages/core/src/components/Link/index.stories.tsx
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-import { Link } from './Link';
-
-storiesOf('Core', module).add('Link', () => <Link href="#">Add New Pharmacy</Link>);
