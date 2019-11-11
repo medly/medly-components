85cd01c0a4586e65de35210b39ceb5c8f5eac66f
gmukul01 <mukulbansal01@gmail.com>
2019-11-11 15:50:35 +0530

    docs(core): update docs for list component
    affects: @medly-components/core

---
+++ packages/core/src/components/List/List.stories.tsx
@@ -0,0 +1,15 @@
+import { select } from '@storybook/addon-knobs';
+import React from 'react';
+import Text from '../Text';
+import { List } from './List';
+import { Props } from './types';
+
+const variant: Array<Props['variant']> = ['horizontal', 'vertical'];
+
+export const Basic = () => (
+    <List variant={select('Variant', variant, 'vertical')}>
+        <Text> Item 1</Text>
+        <Text> Item 2</Text>
+        <Text> Item 3</Text>
+    </List>
+);
