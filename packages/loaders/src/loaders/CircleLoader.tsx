31ad897af5be24565dfd663a9df300c1087d4cba
gmukul01 <mukulbansal01@gmail.com>
2019-12-25 18:10:28 +0530

    perf(loaders): refactor loader components to increase performance
    affects: @medly-components/loaders

--- packages/loaders/src/loaders/CircleLoader.tsx
+++ packages/loaders/src/loaders/CircleLoader.tsx
@@ -1,14 +1,13 @@
-// tslint:disable:max-line-length
 import { WithStyle } from '@medly-components/utils';
 import React, { SFC } from 'react';
 import CircleLoaderSvg from '../assets/CircleLoader.svg';
 import SvgLoader, { Props } from '../SvgLoader';
 
-export const CircleLoader: SFC<Props> & WithStyle = props => (
+export const CircleLoader: SFC<Props> & WithStyle = React.memo(props => (
     <SvgLoader {...props}>
         <CircleLoaderSvg {...props} width="1em" height="1em" />
     </SvgLoader>
-);
+));
 
 CircleLoader.Style = SvgLoader;
 CircleLoader.displayName = 'CircleLoader';
--- packages/loaders/src/loaders/DotsLoader.tsx
+++ packages/loaders/src/loaders/DotsLoader.tsx
@@ -1,14 +1,13 @@
-// tslint:disable:max-line-length
 import { WithStyle } from '@medly-components/utils';
 import React, { SFC } from 'react';
 import DotsLoaderSvg from '../assets/DotsLoader.svg';
 import SvgLoader, { Props } from '../SvgLoader';
 
-export const DotsLoader: SFC<Props> & WithStyle = props => (
+export const DotsLoader: SFC<Props> & WithStyle = React.memo(props => (
     <SvgLoader {...props}>
         <DotsLoaderSvg {...props} width="1em" height="1em" />
     </SvgLoader>
-);
+));
 
 DotsLoader.Style = SvgLoader;
 DotsLoader.displayName = 'DotsLoader';
