d11ae083c1ccf1ce4d43c566071b1c3e674a28ec
gmukul01 <mukulbansal01@gmail.com>
2020-01-15 21:40:16 +0530

    fix(icons): fix audit icon issue
    affects: @medly-components/icons

--- packages/icons/src/icons/AuditIcon.tsx
+++ packages/icons/src/icons/AuditIcon.tsx
@@ -1,12 +1,13 @@
-import { SvgIcon, SvgIconProps } from '@medly-components/icons';
 import { WithStyle } from '@medly-components/utils';
 import React, { SFC } from 'react';
 import AuditSvg from '../assets/Audit.svg';
+import SvgIcon, { Props } from '../SvgIcon';
 
-export const AuditIcon: SFC<SvgIconProps> & WithStyle = React.memo(props => (
+export const AuditIcon: SFC<Props> & WithStyle = React.memo(props => (
     <SvgIcon {...props}>
         <AuditSvg {...props} width="1em" height="1em" />
     </SvgIcon>
 ));
+
 AuditIcon.Style = SvgIcon;
-AuditIcon.displayName = 'Audit';
+AuditIcon.displayName = 'AuditIcon';
