87a7feb6333c139e091b94676c1a8af2f066b70c
Bijal Parekh <i-bijalbkp@Bijals-MacBook-Pro.local>
2020-06-01 21:41:51 +0530

    style: style changes for toast component as per new design
    affects: @medly-components/core, @medly-components/theme

--- packages/theme/src/core/toast/index.ts
+++ packages/theme/src/core/toast/index.ts
@@ -2,11 +2,20 @@ import colors from '../colors';
 import { ToastTheme } from './types';
 
 const toast: ToastTheme = {
-    successColor: colors.green[200],
-    infoColor: colors.communityBlue[200],
-    warningColor: colors.sunshineYellow[200],
-    errorColor: colors.red[200],
-    textColor: colors.professionalNavy
+    textColor: colors.professionalNavy,
+    boxShadow: colors.grey[400],
+    bgColor: {
+        success: colors.green[200],
+        info: colors.communityBlue[200],
+        warning: colors.sunshineYellow[200],
+        error: colors.red[200]
+    },
+    fillColor: {
+        success: colors.green[500],
+        info: colors.communityBlue[500],
+        warning: colors.sunshineYellow[500],
+        error: colors.red[500]
+    }
 };
 
 export default toast;
