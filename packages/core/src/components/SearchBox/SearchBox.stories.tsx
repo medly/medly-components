6223bb1821e8370b1e5d470019009909c5426c54
Bj Wick <tyrion1920@gmail.com>
2020-06-07 15:53:59 +0530

    docs: add storybook doc changes for search box
    affects: @medly-components/core

--- packages/core/src/components/SearchBox/SearchBox.stories.tsx
+++ packages/core/src/components/SearchBox/SearchBox.stories.tsx
@@ -1,4 +1,8 @@
+import { select } from '@storybook/addon-knobs';
 import React from 'react';
 import { SearchBox } from './SearchBox';
+import { Props } from './types';
 
-export const Basic = () => <SearchBox placeholder="Search" />;
+const boxSize: Props['boxSize'][] = ['S', 'M'];
+
+export const Basic = () => <SearchBox placeholder="Search" boxSize={select('Size', boxSize, 'S')} />;
