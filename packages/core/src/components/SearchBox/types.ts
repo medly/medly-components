185a184d416620f9a593074468bd32e53056bf21
Bj Wick <tyrion1920@gmail.com>
2020-06-07 14:29:55 +0530

    docs: add document for search box
    affects: @medly-components/core

--- packages/core/src/components/SearchBox/types.ts
+++ packages/core/src/components/SearchBox/types.ts
@@ -2,8 +2,8 @@ import { SearchBoxSizes } from '@medly-components/theme';
 import { HTMLProps, WithThemeProp } from '@medly-components/utils';
 
 export interface Props extends Omit<HTMLProps<HTMLButtonElement>, 'boxSize'>, WithThemeProp {
-    /** Search box placeholder text */
+    /** Search box placeholder text (optional) */
     placeholder?: string;
-    /** Search box sizes */
+    /** Search box sizes (optional) */
     boxSize?: SearchBoxSizes;
 }
