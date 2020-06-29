3f1b9d63d471ee361e066a0b223499f5fcef7a97
gmukul01 <mukulbansal01@gmail.com>
2020-06-29 11:35:56 +0530

    fix: fix sonar duplicate line issue
    affects: @medly-components/core

--- packages/core/src/components/Tabs/TabList/TabList.test.tsx
+++ packages/core/src/components/Tabs/TabList/TabList.test.tsx
@@ -4,75 +4,64 @@ import Tab from '../Tab';
 import { TabList } from './TabList';
 
 const renderer = (active = 'tab1', onChange = jest.fn()) =>
-    render(
-        <TabList active={active} onChange={onChange}>
-            <Tab id="tab1" label="Add">
-                Content for the add panel
-            </Tab>
-            <Tab id="tab2" label="Edit">
-                Content for the edit panel
-            </Tab>
-        </TabList>
-    );
+        render(
+            <TabList active={active} onChange={onChange}>
+                <Tab id="tab1" label="Add">
+                    Content for the add panel
+                </Tab>
+                <Tab id="tab2" label="Edit">
+                    Content for the edit panel
+                </Tab>
+            </TabList>
+        ),
+    keyboardNavigation = (active: string, label: string, key: string, onChange = jest.fn()) => {
+        const { container } = renderer(active, onChange);
+        fireEvent.focus(screen.getByText(label));
+        fireEvent.keyDown(container, { key });
+    };
 
 describe('TabList', () => {
+    const mockOnChange = jest.fn();
+    afterEach(() => mockOnChange.mockClear());
+
     it('should render properly', () => {
         const { container } = renderer();
         expect(container).toMatchSnapshot();
     });
 
     it('should call onChange prop on click on any tab', () => {
-        const mockOnChange = jest.fn();
         renderer('tab1', mockOnChange);
         fireEvent.click(screen.getByText('Edit'));
         expect(mockOnChange).toBeCalledWith('tab2');
     });
 
     it('should call onChange prop with expected data on pressing right arrow key', () => {
-        const mockOnChange = jest.fn(),
-            { container } = renderer('tab1', mockOnChange);
-        fireEvent.focus(screen.getByText('Add'));
-        fireEvent.keyDown(container, { key: 'ArrowRight' });
+        keyboardNavigation('tab1', 'Add', 'ArrowRight', mockOnChange);
         expect(mockOnChange).toBeCalledWith('tab2');
     });
 
     it('should call onChange prop with first tab id on pressing right arrow key and last tab is active', () => {
-        const mockOnChange = jest.fn(),
-            { container } = renderer('tab2', mockOnChange);
-        fireEvent.focus(screen.getByText('Add'));
-        fireEvent.keyDown(container, { key: 'ArrowRight' });
+        keyboardNavigation('tab2', 'Add', 'ArrowRight', mockOnChange);
         expect(mockOnChange).toBeCalledWith('tab1');
     });
 
     it('should call onChange prop with expected data on pressing left arrow key', () => {
-        const mockOnChange = jest.fn(),
-            { container } = renderer('tab2', mockOnChange);
-        fireEvent.focus(screen.getByText('Add'));
-        fireEvent.keyDown(container, { key: 'ArrowLeft' });
+        keyboardNavigation('tab2', 'Add', 'ArrowLeft', mockOnChange);
         expect(mockOnChange).toBeCalledWith('tab1');
     });
 
     it('should call onChange prop with last tab id on pressing left arrow key and last tab is active', () => {
-        const mockOnChange = jest.fn(),
-            { container } = renderer('tab1', mockOnChange);
-        fireEvent.focus(screen.getByText('Add'));
-        fireEvent.keyDown(container, { key: 'ArrowLeft' });
+        keyboardNavigation('tab1', 'Add', 'ArrowLeft', mockOnChange);
         expect(mockOnChange).toBeCalledWith('tab2');
     });
 
     it('should call onChange prop with expected data on pressing Home key', () => {
-        const mockOnChange = jest.fn(),
-            { container } = renderer('tab2', mockOnChange);
-        fireEvent.focus(screen.getByText('Add'));
-        fireEvent.keyDown(container, { key: 'Home' });
+        keyboardNavigation('tab2', 'Add', 'Home', mockOnChange);
         expect(mockOnChange).toBeCalledWith('tab1');
     });
 
     it('should call onChange prop with expected data on pressing End arrow key', () => {
-        const mockOnChange = jest.fn(),
-            { container } = renderer('tab1', mockOnChange);
-        fireEvent.focus(screen.getByText('Add'));
-        fireEvent.keyDown(container, { key: 'End' });
+        keyboardNavigation('tab1', 'Add', 'End', mockOnChange);
         expect(mockOnChange).toBeCalledWith('tab2');
     });
 });
