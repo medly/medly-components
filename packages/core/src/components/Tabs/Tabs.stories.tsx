a4ecd3275922cf3a16d116f4a0765ba1533a8277
gmukul01 <mukulbansal01@gmail.com>
2020-06-29 09:54:13 +0530

    docs: removed unwanted stories from tabs component
    affects: @medly-components/core

--- packages/core/src/components/Tabs/Tabs.stories.tsx
+++ packages/core/src/components/Tabs/Tabs.stories.tsx
@@ -1,19 +1,18 @@
 import { defaultTheme, TabsTheme } from '@medly-components/theme';
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
-import { Props } from './types';
+import { TabBackground, TabSize, TabStyle } from './types';
 
 export const ThemeInterface: React.FC<TabsTheme> = () => null;
 ThemeInterface.defaultProps = {
     ...defaultTheme.tabs
 };
 
-export const sizes: Props['tabSize'][] = ['S', 'M', 'L'];
-export const tabBg: Props['tabBackground'][] = ['WHITE', 'GREY'];
-export const tabStyle: Props['tabStyle'][] = ['OPEN', 'CLOSED'];
+export const sizes: TabSize[] = ['S', 'M', 'L'];
+export const tabBg: TabBackground[] = ['WHITE', 'GREY'];
+export const tabStyle: TabStyle[] = ['OPEN', 'CLOSED'];
 
-export const DarkBackground = createGlobalStyle`
-    #root, #story--core-tabs--basic, #story--tabs--with-grey-background {
-        background: rgba(229, 229, 229, 0.3);
-    }
+export const DarkBackground = createGlobalStyle<{ withGreyBackground: TabBackground }>`
+#root, #story--core-tabs--basic, #story--tabs--with-grey-background {
+    background: ${({ withGreyBackground }) => withGreyBackground === 'GREY' && 'rgba(229, 229, 229, 0.3)'} }
 `;
