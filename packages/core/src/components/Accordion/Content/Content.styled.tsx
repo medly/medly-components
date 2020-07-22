85343cfa45e1b649cc30de348fb799aaa485de9b
gmukul01 <mukulbansal01@gmail.com>
2020-07-22 15:46:41 +0530

    feat: improve transition in accordion component
    affects: @medly-components/core

--- packages/core/src/components/Accordion/Content/Content.styled.tsx
+++ packages/core/src/components/Accordion/Content/Content.styled.tsx
@@ -2,7 +2,7 @@ import { styled } from '@medly-components/utils';
 
 export const Wrapper = styled('div')<{ isActive: boolean }>`
     overflow: hidden;
-    transition: all 200ms ease-out;
+    transition: all 200ms ${({ isActive }) => (isActive ? 'ease-in' : 'ease-out')};
     max-height: ${({ isActive }) => (isActive ? '100vh' : 0)};
     opacity: ${({ isActive }) => (isActive ? 1 : 0)};
     border-bottom-left-radius: 0.8rem;
