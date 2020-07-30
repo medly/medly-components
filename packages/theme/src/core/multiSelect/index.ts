fef6cf701049a002dd3de79b389ad78dc3db12ec
ram <blitz.krieg10492@gmail.com>
2020-07-30 15:28:01 +0530

    fix: fix sonarCloud issue
    affects: @medly-components/theme

--- packages/theme/src/core/multiSelect/index.ts
+++ packages/theme/src/core/multiSelect/index.ts
@@ -7,89 +7,76 @@ const white = colors.white;
 const red = colors.red;
 const blue = colors.blue;
 
+const iconStyles = {
+    default: {
+        background: grey[300],
+        hoverBackground: grey[400],
+        color: grey[900],
+        hoverColor: black
+    },
+    active: {
+        background: blue[500],
+        hoverBackground: blue[600],
+        color: white,
+        hoverColor: white
+    },
+    error: {
+        background: red[500],
+        hoverBackground: red[600],
+        color: white,
+        hoverColor: white
+    },
+    disabled: {
+        background: grey[100],
+        hoverBackground: grey[100],
+        color: grey[400],
+        hoverColor: grey[400]
+    }
+};
+
+const chipStyles = {
+    default: {
+        background: grey[100],
+        border: grey[400],
+        hoverBorder: grey[600],
+        color: grey[900]
+    },
+    active: {
+        background: blue[100],
+        border: blue[500],
+        hoverBorder: blue[600],
+        color: grey[900]
+    },
+    error: {
+        background: red[100],
+        border: red[500],
+        hoverBorder: red[600],
+        color: grey[900]
+    },
+    disabled: {
+        background: 'transparent',
+        border: grey[300],
+        hoverBorder: grey[300],
+        color: grey[800]
+    }
+};
+
 const multiSelectTheme: MultiSelectTheme = {
     outlined: {
-        icon: {
-            default: {
-                background: grey[300],
-                hoverBackground: grey[400],
-                color: grey[900],
-                hoverColor: black
-            },
-            active: {
-                background: blue[500],
-                hoverBackground: blue[600],
-                color: white,
-                hoverColor: white
-            },
-            error: {
-                background: red[500],
-                hoverBackground: red[600],
-                color: white,
-                hoverColor: white
-            },
-            disabled: {
-                background: grey[100],
-                hoverBackground: grey[100],
-                color: grey[400],
-                hoverColor: grey[400]
-            }
-        },
-        chip: {
-            default: {
-                background: grey[100],
-                border: grey[400],
-                hoverBorder: grey[600],
-                color: grey[900]
-            },
-            active: {
-                background: blue[100],
-                border: blue[500],
-                hoverBorder: blue[600],
-                color: grey[900]
-            },
-            error: {
-                background: red[100],
-                border: red[500],
-                hoverBorder: red[600],
-                color: grey[900]
-            },
-            disabled: {
-                background: white,
-                border: grey[300],
-                hoverBorder: grey[300],
-                color: grey[800]
-            }
-        }
+        icon: iconStyles,
+        chip: chipStyles
     },
     filled: {
         icon: {
+            ...iconStyles,
             default: {
+                ...iconStyles.default,
                 background: grey[400],
-                hoverBackground: grey[500],
-                color: grey[900],
-                hoverColor: black
-            },
-            active: {
-                background: blue[500],
-                hoverBackground: blue[600],
-                color: white,
-                hoverColor: white
-            },
-            error: {
-                background: red[500],
-                hoverBackground: red[600],
-                color: white,
-                hoverColor: white
-            },
-            disabled: {
-                background: grey[100],
-                hoverBackground: grey[100],
-                color: grey[400],
-                hoverColor: grey[400]
+                hoverBackground: grey[500]
             }
         },
         chip: {
+            ...chipStyles,
             default: {
                 background: grey[200],
                 border: grey[600],
@@ -97,22 +84,12 @@ const multiSelectTheme: MultiSelectTheme = {
                 color: grey[900]
             },
             active: {
-                background: 'rgba(0, 90, 238, 0.2)',
-                border: blue[500],
-                hoverBorder: blue[600],
-                color: grey[900]
+                ...chipStyles.active,
+                background: 'rgba(0, 90, 238, 0.2)'
             },
             error: {
-                background: 'rgba(204, 0, 0, 0.2)',
-                border: red[500],
-                hoverBorder: red[600],
-                color: grey[900]
-            },
-            disabled: {
-                background: 'transparent',
-                border: grey[300],
-                hoverBorder: grey[300],
-                color: grey[800]
+                ...chipStyles.error,
+                background: 'rgba(204, 0, 0, 0.2)'
             }
         }
     }
