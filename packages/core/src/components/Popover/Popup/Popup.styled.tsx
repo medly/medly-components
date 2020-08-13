8f36bb04bd214783eb019029bdee61dd24463b2c
gmukul01 <mukulbansal01@gmail.com>
2020-08-13 20:16:21 +0530

    feat: add distance prop in the popover component
    affects: @medly-components/core

--- packages/core/src/components/Popover/Popup/Popup.styled.tsx
+++ packages/core/src/components/Popover/Popup/Popup.styled.tsx
@@ -1,70 +1,70 @@
 import { css, fullHeight, fullWidth, styled } from '@medly-components/utils';
 import { PopupProps } from './types';
 
-export const getPosition = ({ placement }: Partial<PopupProps>) => {
+export const getPosition = ({ placement, distance }: Partial<PopupProps>) => {
     switch (placement) {
         case 'top-start':
             return css`
-                bottom: calc(100% + 5px);
+                bottom: calc(100% + ${distance || '0px'});
                 left: 0%;
             `;
         case 'top':
             return css`
-                bottom: calc(100% + 5px);
+                bottom: calc(100% + ${distance || '0px'});
                 left: 50%;
                 transform: translateX(-50%);
             `;
         case 'top-end':
             return css`
-                bottom: calc(100% + 5px);
+                bottom: calc(100% + ${distance || '0px'});
                 right: 0%;
             `;
         case 'right-start':
             return css`
                 top: 0%;
-                left: calc(100% + 5px);
+                left: calc(100% + ${distance || '0px'});
             `;
         case 'right':
             return css`
-                left: calc(100% + 5px);
+                left: calc(100% + ${distance || '0px'});
                 top: 50%;
                 transform: translateY(-50%);
             `;
         case 'right-end':
             return css`
                 bottom: 0;
-                left: calc(100% + 5px);
+                left: calc(100% + ${distance || '0px'});
             `;
         case 'bottom-start':
             return css`
-                top: calc(100% + 5px);
+                top: calc(100% + ${distance || '0px'});
                 left: 0%;
             `;
         case 'bottom':
             return css`
-                top: calc(100% + 5px);
+                top: calc(100% + ${distance || '0px'});
                 left: 50%;
                 transform: translateX(-50%);
             `;
         case 'bottom-end':
             return css`
-                top: calc(100% + 5px);
+                top: calc(100% + ${distance || '0px'});
                 right: 0%;
             `;
         case 'left-start':
             return css`
-                right: calc(100% + 5px);
+                right: calc(100% + ${distance || '0px'});
                 top: 0%;
             `;
         case 'left':
             return css`
-                right: calc(100% + 5px);
+                right: calc(100% + ${distance || '0px'});
                 top: 50%;
                 transform: translateY(-50%);
             `;
         case 'left-end':
             return css`
-                right: calc(100% + 5px);
+                right: calc(100% + ${distance || '0px'});
                 bottom: 0%;
             `;
     }
--- packages/core/src/components/Popover/Popup/types.ts
+++ packages/core/src/components/Popover/Popup/types.ts
@@ -2,6 +2,8 @@ import { HTMLProps } from '@medly-components/utils';
 import { Placement } from '../types';
 
 export interface PopupProps extends HTMLProps<HTMLDivElement> {
+    /** distance from content */
+    distance?: string;
     /** Takes full width of the parent component */
     fullWidth?: boolean;
     /** Takes full height of the parent component */
