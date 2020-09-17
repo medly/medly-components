6081ec51f8bf66b24d56b74bb016a1bc726199fe
dhirendra777 <dhirendra.naik@medlypharmacy.com>
2020-09-18 00:15:51 +0530

    fix(core): fix SearchBox onEnter search trigger issue
    affects: @medly-components/core

--- packages/core/src/components/SearchBox/SearchBox.test.tsx
+++ packages/core/src/components/SearchBox/SearchBox.test.tsx
@@ -42,6 +42,7 @@ describe('SearchBox', () => {
             withOptionCB = { placeholder: 'search', onInputChange: jest.fn(), onSearch };
 
         const { container, inputEl } = renderComponent(withOptionCB);
+        inputEl.focus();
         fireEvent.change(inputEl, { target: { value: 'Dummy' } });
         fireEvent.keyDown(container, { key: 'Enter', code: 13 });
         expect(onSearch).toHaveBeenCalledWith('Dummy');
