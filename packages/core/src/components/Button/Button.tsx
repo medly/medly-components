dac86720945cca51b35f1815fb0a664f69dcc447
gmukul01 <mukulbansal01@gmail.com>
2020-09-30 16:03:09 +0530

    feat: move padding and textVariant to theme in button component
    affects: @medly-components/core, @medly-components/forms, @medly-components/theme,
    @medly-components/utils

--- packages/core/src/components/Button/Button.tsx
+++ packages/core/src/components/Button/Button.tsx
@@ -1,17 +1,20 @@
-import { isValidStringOrNumber, WithStyle } from '@medly-components/utils';
-import React, { FC } from 'react';
+import { isValidStringOrNumber, ThemeContext, WithStyle } from '@medly-components/utils';
+import React, { FC, useContext } from 'react';
 import Text from '../Text';
 import { ButtonStyled } from './Button.styled';
 import { Props } from './types';
 
 export const Button: FC<Props> & WithStyle = React.memo(
-    React.forwardRef((props, ref) => (
-        <ButtonStyled ref={ref} {...props}>
-            {React.Children.map(props.children, c => {
-                return isValidStringOrNumber(c) ? <Text textVariant={props.size === 'M' ? 'button1' : 'button2'}>{c}</Text> : c;
-            })}
-        </ButtonStyled>
-    ))
+    React.forwardRef((props, ref) => {
+        const theme = useContext(ThemeContext);
+        return (
+            <ButtonStyled ref={ref} {...props}>
+                {React.Children.map(props.children, c => {
+                    return isValidStringOrNumber(c) ? <Text textVariant={theme.button.textVariant[props.size]}>{c}</Text> : c;
+                })}
+            </ButtonStyled>
+        );
+    })
 );
 Button.defaultProps = {
     type: 'button',
