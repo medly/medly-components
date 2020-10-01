4adf0c7fdfd2f14fc1e2844dac85626b143a8e81
gmukul01 <mukulbansal01@gmail.com>
2020-10-01 09:30:17 +0530

    feat: add shouldCloseOnOutsideClick prop in modal component
    affects: @medly-components/core

--- packages/core/src/components/Modal/types.ts
+++ packages/core/src/components/Modal/types.ts
@@ -7,10 +7,12 @@ export interface Props extends HTMLProps<HTMLDivElement>, WithThemeProp {
     open?: boolean;
     /** Function to be called on closing modal */
     onCloseModal?: () => void;
-    /** Min width in px/rem/% (1rem = 10px)*/
+    /** Min width in px/rem/% (1rem = 10px) */
     minWidth?: string;
-    /** Min height in px/rem/% (1rem = 10px)*/
+    /** Min height in px/rem/% (1rem = 10px) */
     minHeight?: string;
+    /** Set it true to close the modal on click outside of popup */
+    shouldCloseOnOutsideClick?: boolean;
 }
 
 export interface ModalStaticProps {
