aeca708f9a60c9386a134eef7a37db0ba4ab49a3
Sachidananda Panigrahi <snpanigrahi88@gmail.com>
2020-10-06 11:06:31 +0530

    add month style and functionaity changes

--- packages/core/src/components/DateRangePicker/DateRangeCalendar/Month/Month.styled.tsx
+++ packages/core/src/components/DateRangePicker/DateRangeCalendar/Month/Month.styled.tsx
@@ -1,28 +1,87 @@
-import { css, styled } from '@medly-components/utils';
+import { styled } from '@medly-components/utils';
 import * as CalendarStyled from '../../../Calendar/Calendar.styled';
 import Text from '../../../Text';
 
-const hideDateStyle = css`
+export const CalendarGrid = styled(CalendarStyled.CalendarGrid)`
+    button.selectedStartDate,
+    button.selectedEndDate {
+        /* margin: 0.4rem 0; */
+
+        /* height: 32px; */
+
+        /* width: 32px; */
+
+        /* position: relative;
+        &::before {
+            position: absolute;
+            content: '';
+            z-index: -1;
+            right: -6px;
+            top: -2px;
+            width: 18px;
+            height: 32px;
+            background-color: ${({ theme }) => theme.colors.blue[100]};
+        } */
+    }
+    button.selectedEndDate {
+        /* &::before {
+            right: unset;
+            left: -6px;
+        } */
+    }
+
+    button.hide {
         opacity: 0;
         cursor: default;
         pointer-events: none;
-    `,
-    dateBetweenRangeStyle = css`
-        background-color: ${({ theme }) => theme.colors.blue[100]};
+    }
+    button.active,
+    button.hoverActive {
         border-radius: 0;
         margin: 0.4rem 0;
-    `;
-
-export const Date = styled(CalendarStyled.Date)<{ isInDateRange?: boolean }>`
-    ${({ isInActiveMonth }) => !isInActiveMonth && hideDateStyle};
-    ${({ isInDateRange }) => isInDateRange && dateBetweenRangeStyle};
+        background-color: ${({ theme }) => theme.colors.blue[100]};
+    }
+    button.hoverActive {
+        background-color: ${({ theme }) => theme.colors.grey[100]};
+    }
+    button.active.end,
+    button.active.start,
+    button.hoverActive.end,
+    button.hoverActive.start {
+        position: relative;
+        &::after {
+            position: absolute;
+            content: '';
+            z-index: 1;
+            right: -26px;
+            top: -2px;
+            width: 24px;
+            height: 32px;
+            background: linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, ${({ theme }) => theme.colors.grey[100]} 100%);
+            transform: rotate(-180deg);
+        }
+    }
+    button.active.start,
+    button.hoverActive.start {
+        &::after {
+            transform: rotate(0deg);
+            left: -26px;
+            right: unset;
+        }
+    }
+    button.active.end,
+    button.active.start {
+        &::after {
+            background: linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, ${({ theme }) => theme.colors.blue[100]} 100%);
+        }
+    }
 `;
 
 export const Wrapper = styled.div`
     position: relative;
     padding-top: 2.3rem;
     ${CalendarStyled.CalendarGrid} {
-        padding: 1.6rem;
+        padding: 1.6rem 3.2rem;
     }
     &:last-child {
         border-left: 1px solid #dfe4e9;
--- packages/core/src/components/DateRangePicker/DateRangeCalendar/Month/Month.tsx
+++ packages/core/src/components/DateRangePicker/DateRangeCalendar/Month/Month.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import * as CalendarStyled from '../../../Calendar/Calendar.styled';
 import { LONG_CALENDAR_MONTHS } from '../../../Calendar/constants';
 import { getCalendarDates, isSameDay, isSameMonth } from '../../../Calendar/helper';
@@ -8,46 +8,84 @@ import * as Styled from './Month.styled';
 import { Props } from './types';
 
 export const Month: React.FC<Props> = React.memo(
-    ({ id, startDate, endDate, month, year, onChange, minSelectableDate, maxSelectableDate, ...restProps }) => {
+    ({
+        id,
+        startDate,
+        endDate,
+        month,
+        year,
+        onChange,
+        minSelectableDate,
+        maxSelectableDate,
+        hoveredDate,
+        setHoveredDate,
+        ...restProps
+    }) => {
         const today = new Date(),
-            [hoveredDate, setHoveredDate] = useState<Date | null>(null);
-
-        const handleDateChange = useCallback((dt: Date) => () => onChange(dt), [onChange]),
-            handleMouseOver = useCallback((dt: Date) => () => setHoveredDate(dt), []);
+            handleDateChange = useCallback((dt: Date) => () => onChange(dt), [onChange]),
+            handleMouseOver = useCallback((dt: Date) => () => setHoveredDate(dt), []),
+            getClassNames = useCallback(
+                (
+                    isInActiveMonth: boolean,
+                    isInDateRange: boolean,
+                    isInDateRangeHover: boolean,
+                    isFirstDate: boolean,
+                    isLastDate: boolean,
+                    isSelectedStartDate: boolean,
+                    isSelectedEndDate: boolean
+                ) => {
+                    return isInActiveMonth
+                        ? `${isSelectedStartDate ? 'selectedStartDate' : ''} ${isSelectedEndDate ? 'selectedEndDate' : ''} ${
+                              isInDateRange ? 'active' : ''
+                          } ${isInDateRangeHover ? 'hoverActive' : ''} ${isFirstDate ? 'start' : ''} ${isLastDate ? 'end' : ''}`.trim()
+                        : 'hide';
+                },
+                []
+            );
 
         return (
             <Styled.Wrapper id={id} {...restProps}>
                 <Styled.MonthText id={`${id}-text`} textVariant="button1" textAlign="center">
                     {`${LONG_CALENDAR_MONTHS[month]} ${year}`}
                 </Styled.MonthText>
-                <CalendarStyled.CalendarGrid id={`${id}-grid`}>
+                <Styled.CalendarGrid id={`${id}-grid`}>
                     <WeekDays />
                     {getCalendarDates(month, year).map((dateArray, index) => {
                         const _date = new Date(dateArray[0], dateArray[1], dateArray[2]),
                             isSelected = isSameDay(_date, startDate) || isSameDay(_date, endDate),
-                            isInDateRange =
-                                (startDate && endDate && _date < endDate && _date > startDate) ||
-                                (startDate && !endDate && _date > startDate && _date < hoveredDate),
+                            isSelectedStartDate = isSameDay(_date, startDate),
+                            isSelectedEndDate = isSameDay(_date, endDate),
+                            isInDateRange = startDate && endDate && _date < endDate && _date > startDate,
+                            isInDateRangeHover = startDate && !endDate && _date > startDate && _date < hoveredDate,
                             isInActiveMonth = isSameMonth(_date, new Date(year, month, 1)),
-                            isCurrentDate = isSameDay(_date, today);
-
+                            isCurrentDate = isSameDay(_date, today),
+                            isFirstDate = new Date(year, month, 1).getDate() === _date.getDate(),
+                            isLastDate = new Date(year, month + 1, 0).getDate() === _date.getDate();
                         return (
-                            <Styled.Date
+                            <CalendarStyled.Date
                                 key={index}
+                                className={getClassNames(
+                                    isInActiveMonth,
+                                    isInDateRange,
+                                    isInDateRangeHover,
+                                    isFirstDate,
+                                    isLastDate,
+                                    isSelectedStartDate,
+                                    isSelectedEndDate
+                                )}
                                 title={_date.toDateString()}
                                 isInActiveMonth={isInActiveMonth}
                                 isSelected={isSelected}
                                 isCurrentDate={isCurrentDate}
-                                isInDateRange={isInDateRange}
                                 onMouseOver={handleMouseOver(_date)}
                                 disabled={_date > maxSelectableDate || _date < minSelectableDate}
                                 onClick={handleDateChange(_date)}
                             >
                                 <Text>{_date.getDate()}</Text>
-                            </Styled.Date>
+                            </CalendarStyled.Date>
                         );
                     })}
-                </CalendarStyled.CalendarGrid>
+                </Styled.CalendarGrid>
             </Styled.Wrapper>
         );
     }
