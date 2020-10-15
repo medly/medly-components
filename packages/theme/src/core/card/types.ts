c0b209c2957bc07b50537d38684bd15885838163
gmukul01 <mukulbansal01@gmail.com>
2020-10-16 00:19:25 +0530

    feat: move border radius and padding to them of card component
    affects: @medly-components/core, @medly-components/theme

--- packages/theme/src/core/card/types.ts
+++ packages/theme/src/core/card/types.ts
@@ -1,5 +1,11 @@
+import { BreakPointsTheme } from '../breakPoints/types';
+
 export interface CardTheme {
     backgroundColor: string;
     separatorColor: string;
     shadowColor: string;
+    borderRadius: string;
+    padding: {
+        [k in keyof BreakPointsTheme]: string;
+    };
 }
