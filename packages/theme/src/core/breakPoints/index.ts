55b4640f180f9859135d6f4233932498bc2f9d40
gmukul01 <mukulbansal01@gmail.com>
2020-10-20 23:45:48 +0530

    fix: increase max screen size of XL breakpoint
    affects: @medly-components/core, @medly-components/theme

--- packages/theme/src/core/breakPoints/index.ts
+++ packages/theme/src/core/breakPoints/index.ts
@@ -4,7 +4,7 @@ const breakPoints: BreakPointsTheme = {
     S: { min: 0, max: 767 },
     M: { min: 768, max: 1023 },
     L: { min: 1024, max: 1279 },
-    XL: { min: 1280, max: 1600 }
+    XL: { min: 1280, max: 2400 }
 };
 
 export default breakPoints;
