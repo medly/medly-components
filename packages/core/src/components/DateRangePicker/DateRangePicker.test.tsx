9c1bd17900facac8d47ab26ef9de17b29c88ba2f
Sachidananda Panigrahi <snpanigrahi88@gmail.com>
2020-10-23 20:40:41 +0530

    - add test cases - add code refactor

--- packages/core/src/components/DateRangePicker/DateRangePicker.test.tsx
+++ packages/core/src/components/DateRangePicker/DateRangePicker.test.tsx
@@ -4,48 +4,40 @@ import { placements } from '../Popover/Popover.stories';
 import { DateRangePicker } from './DateRangePicker';
 import { DateRangeProps } from './types';
 
+
+const renderComponent = (props?: any) => {
+    const defaultProps: DateRangeProps = {
+        value: { startDate: null, endDate: null },
+        displayFormat: 'MM/dd/yyyy',
+        id: "contract",
+        required: false,
+        fromLabel: "From",
+        toLabel: "To",
+        size: 'M',
+        popoverPlacement: 'bottom-start',
+        variant: 'filled',
+        fullWidth: false,
+        disabled: false,
+        minSelectableDate: new Date(2020, 1, 1),
+        maxSelectableDate: new Date(2022, 2, 15),
+        onChange: jest.fn()
+    };
+
+    return render(
+        <DateRangePicker
+            id="contract" {...defaultProps} {...props} />);
+};
+
 describe('DateRangePicker', () => {
     afterEach(cleanup);
     it('should render properly when value is of date type', () => {
-        const { container } = render(
-            <DateRangePicker
-                value={{ startDate: new Date(2010, 0, 1), endDate: new Date(2010, 0, 2) }}
-                onChange={jest.fn()}
-                id="contract"
-                displayFormat='MM/dd/yyyy'
-                required={false}
-                fromLabel="From"
-                toLabel="To"
-                size='M'
-                popoverPlacement='bottom-start'
-                variant='filled'
-                fullWidth={false}
-                disabled={false}
-                minSelectableDate={new Date(2020, 1, 1)}
-                maxSelectableDate={new Date(2022, 2, 15)}
-            />
-        );
+        const { container } = renderComponent();
         expect(container).toMatchSnapshot();
     });
 
     describe('popover placement', () => {
         test.each(placements)('should render properly with %p position', (popoverPlacement: DateRangeProps['popoverPlacement']) => {
-            const { container } = render(
-                <DateRangePicker
-                    id="contract"
-                    value={{ startDate: new Date(2010, 0, 1), endDate: new Date(2010, 0, 2) }}
-                    displayFormat="MM/dd/yyyy"
-                    onChange={jest.fn()}
-                    popoverPlacement={popoverPlacement}
-                    required={false}
-                    fromLabel="From"
-                    toLabel="To"
-                    size='M'
-                    variant='filled'
-                    fullWidth={false}
-                    disabled={false}
-                />
-            );
+            const { container } = renderComponent({ popoverPlacement });
             fireEvent.click(container.querySelector('svg'));
             expect(container.querySelector('#contract-calendar')).toBeVisible();
             expect(container.querySelector('#contract-calendar')).toMatchSnapshot();
@@ -54,34 +46,12 @@ describe('DateRangePicker', () => {
 
     describe('calendar', () => {
         it('should show calendar on click on icon', () => {
-            const { container } = render(
-                <DateRangePicker
-                    id="contract"
-                    value={{ startDate: new Date(2010, 0, 1), endDate: new Date(2010, 0, 2) }}
-                    displayFormat="MM/dd/yyyy"
-                    onChange={jest.fn()}
-                    popoverPlacement='bottom-start'
-                    required={false}
-                    fromLabel="From"
-                    toLabel="To"
-                    size='M'
-                    variant='filled'
-                    fullWidth={false}
-                    disabled={false}
-                />
-            );
+            const { container } = renderComponent();
             fireEvent.click(container.querySelector('svg'));
             expect(container.querySelector('#contract-calendar')).toBeVisible();
         });
         it('should not show calendar on click on icon if is disabled prop is passes', () => {
-            const { container } = render(
-                <DateRangePicker
-                    disabled
-                    id="contract"
-                    value={{ startDate: new Date(2010, 0, 1), endDate: new Date(2010, 0, 2) }}
-                    displayFormat="MM/dd/yyyy"
-                    onChange={jest.fn()} />
-            );
+            const { container } = renderComponent({ disabled: true });
             fireEvent.click(container.querySelector('svg'));
             expect(container.querySelector('#contract-calendar')).toBeNull();
         });
@@ -107,62 +77,52 @@ describe('DateRangePicker', () => {
         it('should change call onChange of from date with expected date', () => {
             const mockOnChange = jest.fn(),
                 dateToSelect = { "endDate": new Date("2020-02-01T18:30:00.000Z"), "startDate": new Date("2020-03-01T18:30:00.000Z") },
-                { container } = render(<DateRangePicker
-                    id="contract"
-                    value={{ startDate: new Date('01, 02, 2020'), endDate: new Date('02, 02, 2020') }}
-                    displayFormat="MM/dd/yyyy"
-                    onChange={mockOnChange} />);
+                { container } = renderComponent({
+                    value: { startDate: new Date('01, 02, 2020'), endDate: new Date('02, 02, 2020') },
+                    onChange: mockOnChange
+                });
             fireEvent.change(container.querySelector('#contract-from-input'), { target: { value: '03 / 02 / 2020' } });
             expect(mockOnChange).toHaveBeenCalledWith(dateToSelect);
         });
         it('should change call onChange of to date with expected date', () => {
             const mockOnChange = jest.fn(),
                 dateToSelect = { "endDate": new Date("2020-03-01T18:30:00.000Z"), "startDate": new Date("2020-01-01T18:30:00.000Z") },
-                { container } = render(<DateRangePicker
-                    id="contract"
-                    value={{ startDate: new Date('01, 02, 2020'), endDate: new Date('02, 02, 2020') }}
-                    displayFormat="MM/dd/yyyy"
-                    onChange={mockOnChange} />);
+                { container } = renderComponent({
+                    value: { startDate: new Date('01, 02, 2020'), endDate: new Date('02, 02, 2020') },
+                    onChange: mockOnChange
+                });
             fireEvent.change(container.querySelector('#contract-to-input'), { target: { value: '03 / 02 / 2020' } });
             expect(mockOnChange).toHaveBeenCalledWith(dateToSelect);
         });
         it('should call onChange with null if typed from date is invalid', async () => {
             const mockOnChange = jest.fn(),
-                { container } = render(<DateRangePicker id="contract" value={{ startDate: null, endDate: null }} displayFormat="MM/dd/yyyy" onChange={mockOnChange} />);
+                { container } = renderComponent({
+                    onChange: mockOnChange
+                });
             fireEvent.change(container.querySelector('#contract-from-input'), { target: { value: '01 / 02' } });
             expect(mockOnChange).toHaveBeenCalledTimes(0);
         });
         it('should call onChange with null if typed to date is invalid', async () => {
             const mockOnChange = jest.fn(),
-                { container } = render(<DateRangePicker id="contract" value={{ startDate: null, endDate: null }} displayFormat="MM/dd/yyyy" onChange={mockOnChange} />);
+                { container } = renderComponent({ onChange: mockOnChange });
             fireEvent.change(container.querySelector('#contract-to-input'), { target: { value: '01 / 02' } });
             expect(mockOnChange).toHaveBeenCalledTimes(0);
         });
     });
     describe('error messages', () => {
-        const props: DateRangeProps = {
-            value: { startDate: null, endDate: null },
-            displayFormat: 'MM/dd/yyyy',
-            onChange: jest.fn()
-        };
-        const renderComponent = (required = false, id: string) => {
-            const { container, getByText } = render(<DateRangePicker id="contract" {...props} required={required} />);
-            const inputEl = container.querySelector(`#contract-${id}-input`);
-            return {
-                inputEl,
-                getByText
-            };
-        };
 
-        it('should return error message if from date entered is incomplete', async () => {
-            const { inputEl, getByText } = renderComponent(true, 'from');
+
+        it('should return error message if FROM date entered is incomplete', async () => {
+            const { container, getByText } = renderComponent({ required: true });
+            const inputEl = container.querySelector('#contract-from-input')
             fireEvent.change(inputEl, { target: { value: '04/31' } });
             fireEvent.blur(inputEl);
             await waitFor(() => expect(getByText('Enter valid date')).toBeInTheDocument());
         });
 
-        it('should return error message if to date entered is incomplete', async () => {
-            const { inputEl, getByText } = renderComponent(true, 'to');
+        it('should return error message if TO date entered is incomplete', async () => {
+            const { container, getByText } = renderComponent({ required: true });
+            const inputEl = container.querySelector('#contract-to-input')
             fireEvent.change(inputEl, { target: { value: '04/31' } });
             fireEvent.blur(inputEl);
             await waitFor(() => expect(getByText('Enter valid date')).toBeInTheDocument());
@@ -171,14 +131,10 @@ describe('DateRangePicker', () => {
     describe('Handlers', () => {
         it('should call onChange on selecting date', async () => {
             const mockOnChange = jest.fn(),
-                dateToSelect = { startDate: new Date(2020, 0, 2) },
-                { container, getByTitle } = render(
-                    <DateRangePicker
-                        id="contract"
-                        value={{ startDate: new Date(2020, 0, 1), endDate: new Date(2020, 0, 1) }}
-                        displayFormat="MM/dd/yyyy"
-                        onChange={mockOnChange} />
-                );
+                dateToSelect = { startDate: new Date(2020, 10, 25) },
+                { container, getByTitle } = renderComponent({
+                    onChange: mockOnChange
+                });
             fireEvent.click(container.querySelector('svg'));
             fireEvent.click(getByTitle(dateToSelect.startDate.toDateString()));
             expect(mockOnChange).toHaveBeenCalledWith(dateToSelect);
@@ -188,35 +144,67 @@ describe('DateRangePicker', () => {
             const mockOnFocus = jest.fn(),
                 mockOnChange = jest.fn(),
                 mockOnBlur = jest.fn(),
-                { container } = render(
-                    <DateRangePicker
-                        id="contract"
-                        value={{ startDate: new Date(2020, 0, 1), endDate: new Date(2020, 0, 1) }}
-                        displayFormat="MM/dd/yyyy"
-                        onChange={mockOnChange}
-                        onFocus={mockOnFocus}
-                        onBlur={mockOnBlur}
-                    />
-                ),
-                inputEl = container.querySelector('#contract-from-input') as HTMLInputElement;
+                { container } = renderComponent({
+                    value: { startDate: new Date(2020, 0, 1), endDate: new Date(2020, 0, 1) },
+                    onChange: mockOnChange,
+                    onFocus: mockOnFocus,
+                    onBlur: mockOnBlur
+                }),
+                inputElFrom = container.querySelector('#contract-from-input') as HTMLInputElement,
+                inputElTo = container.querySelector('#contract-to-input') as HTMLInputElement;
             fireEvent.click(container.querySelector('svg'));
             expect(mockOnFocus).toHaveBeenCalled();
-            fireEvent.blur(inputEl);
+            fireEvent.blur(inputElFrom);
             expect(mockOnBlur).toHaveBeenCalled();
+            fireEvent.change(inputElFrom, { target: { value: '10 / 14 / 2020' } });
+            expect(mockOnChange).toHaveBeenCalled();
+            fireEvent.focus(inputElFrom);
+            expect(mockOnFocus).toHaveBeenCalled();
+            fireEvent.focus(inputElTo);
+            expect(mockOnFocus).toHaveBeenCalled();
+            fireEvent.blur(inputElTo);
+            expect(mockOnBlur).toHaveBeenCalled();
+            fireEvent.change(inputElTo, { target: { value: '10 / 18 / 2020' } });
+            expect(mockOnChange).toHaveBeenCalled();
+        });
+
+        it('focused element should change', () => {
+            const mockOnFocus = jest.fn(),
+                mockOnChange = jest.fn(),
+                mockOnBlur = jest.fn(),
+                { container } = renderComponent({
+                    onChange: mockOnChange,
+                    onFocus: mockOnFocus,
+                    onBlur: mockOnBlur
+                }),
+                inputElFrom = container.querySelector('#contract-from-input') as HTMLInputElement,
+                inputElTo = container.querySelector('#contract-to-input') as HTMLInputElement;
+
+            fireEvent.focus(inputElTo);
+            expect(mockOnFocus).toHaveBeenCalled();
+            fireEvent.blur(inputElTo);
+            expect(mockOnBlur).toHaveBeenCalled();
+            fireEvent.change(inputElTo, { target: { value: '10 / 18 / 2020' } });
+            expect(mockOnChange).toHaveBeenCalled();
+
+            expect(mockOnFocus).toHaveBeenCalled();
+            fireEvent.blur(inputElFrom);
+            expect(mockOnBlur).toHaveBeenCalled();
+            fireEvent.change(inputElFrom, { target: { value: '10 / 14 / 2020' } });
+            expect(mockOnChange).toHaveBeenCalled();
+            fireEvent.focus(inputElFrom);
+            expect(mockOnFocus).toHaveBeenCalled();
+
         });
 
         it('should call invalid handler if passed', () => {
             const mockOnInvalid = jest.fn(),
                 mockOnChange = jest.fn(),
-                { container } = render(
-                    <DateRangePicker
-                        id="contract"
-                        value={{ startDate: new Date(2020, 0, 1), endDate: new Date(2020, 0, 1) }}
-                        displayFormat="MM/dd/yyyy"
-                        onChange={mockOnChange}
-                        onInvalid={mockOnInvalid}
-                    />
-                ),
+                { container } = renderComponent({
+                    value: { startDate: new Date(2020, 0, 1), endDate: new Date(2020, 0, 1) },
+                    onChange: mockOnChange,
+                    onInvalid: mockOnInvalid
+                }),
                 inputEl = container.querySelector('#contract-from-input') as HTMLInputElement;
             fireEvent.click(container.querySelector('svg'));
             fireEvent.invalid(inputEl);
@@ -225,25 +213,22 @@ describe('DateRangePicker', () => {
     });
     describe('Styles', () => {
         it('should change the margin and width if full-width is passed as prop', () => {
-            const { container } = render(
-                <DateRangePicker id="contract"
-                    value={{ startDate: null, endDate: null }}
-                    displayFormat="MM/dd/yyyy"
-                    onChange={jest.fn()}
-                    fullWidth={true} />
-            );
+            const { container } = renderComponent({
+                value: { startDate: null, endDate: null },
+                onChange: jest.fn(),
+                fullWidth: true
+            });
             expect(container.querySelector('#contract-wrapper')).toHaveStyle(`width: 100%`);
         });
 
-        /* it('should change the size of DateRangePicker based on size prop', async () => {
-            const { container } = render(<DateRangePicker
-                id="contract"
-                value={{ startDate: null, endDate: null }}
-                displayFormat="MM/dd/yyyy"
-                size={'S'}
-                onChange={jest.fn()} />);
+        it('should change the size of DateRangePicker based on size prop', async () => {
+            const { container } = renderComponent({
+                size: 'S',
+                onChange: jest.fn()
+            });
             fireEvent.click(container.querySelector('svg'));
             await waitFor(() => expect(container.querySelector('#contract-calendar')).toHaveStyle(`top: 4rem`));
-        }); */
+            // expect(container.querySelector('#contract-calendar')).toMatchSnapshot();
+        });
     });
 });
