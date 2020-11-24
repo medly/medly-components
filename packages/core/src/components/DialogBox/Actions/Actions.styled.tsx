cf9b94dd52a99c8d93d9f343628583bf8f5a6c0f
minheekangg <minheekangg@gmail.com>
2020-11-24 12:20:23 -0500

    feat(core): adjust flex-direction for actions
    affects: @medly-components/core

--- packages/core/src/components/DialogBox/Actions/Actions.styled.tsx
+++ packages/core/src/components/DialogBox/Actions/Actions.styled.tsx
@@ -8,11 +8,18 @@ export const Actions = styled('div')<StyledProps>`
     padding: ${({ theme }) => `${theme.spacing.M1} ${theme.spacing.S4} 0`};
     box-shadow: ${({ theme, scrollState, isSmallScreen }) =>
         !scrollState.scrolledToBottom && !isSmallScreen && `0 -1.8rem 1.6rem -1.6rem ${rgba(theme.colors.grey[400], 0.6)}`};
-    flex-direction: row;
     justify-content: ${({ alignItems }) => (alignItems === 'left' ? 'flex-start' : alignItems === 'center' ? 'center' : 'flex-end')};
+    flex-direction: column;
 
     & > * + * {
-        margin-left: 1.6rem;
+        margin: 1.6rem 0;
+    }
+    @media (min-width: 768px) {
+        flex-direction: row;
+        
+        & > * + * {
+            margin: 0 0 0 1.6rem;
+        }
     }
 `;
 Actions.defaultProps = {
