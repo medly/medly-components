babd395330a563248513d67d986cce33a1a7ebf2
gmukul01 <mukulbansal01@gmail.com>
2020-12-07 16:06:33 +0530

    test: add tests for table's pagination
    affects: @medly-components/core

---
+++ packages/core/src/components/Table/Table.test.tsx
@@ -0,0 +1,35 @@
+import { fireEvent, render, screen } from '@test-utils';
+import React from 'react';
+import testColumns from './docs/columns';
+import testData from './docs/data';
+import { Table } from './Table';
+import { TableProps } from './types';
+
+const renderTable = (props?: Partial<TableProps>) => render(<Table data={testData} columns={testColumns} {...props} />);
+
+describe('Table component', () => {
+    it('should render properly', () => {
+        const { container } = renderTable();
+        expect(container).toMatchSnapshot();
+    });
+
+    describe('pagination', () => {
+        it('should call onPageChange prop on click on any page', () => {
+            const mockOnPageChange = jest.fn();
+            const { container } = renderTable({
+                enablePagination: true,
+                totalItems: 300,
+                itemsPerPage: 10,
+                defaultActivePage: 2,
+                onPageChange: mockOnPageChange
+            });
+            expect(container).toMatchSnapshot();
+            // @ts-ignore
+            fireEvent.click(screen.getByRole('heading', { level: 5, name: '3' }));
+            expect(mockOnPageChange).toBeCalledWith({
+                activePage: 3,
+                sortOrder: 'desc'
+            });
+        });
+    });
+});
