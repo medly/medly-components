88c9a15accefe3ec5b05df3e3b14046033052742
vaibhavee shinde <vaibhaveemv11@gmail.com>
2021-03-01 22:55:12 +0530

    docs(core): renaming size & labelPosition to sizes & labelPositions
    affects: @medly-components/core

--- packages/core/src/components/Toggle/Toggle.stories.tsx
+++ packages/core/src/components/Toggle/Toggle.stories.tsx
@@ -2,8 +2,8 @@ import { defaultTheme, ToggleTheme } from '@medly-components/theme';
 import React from 'react';
 import { Props } from './types';
 
-export const labelPosition: Props['labelPosition'][] = ['left', 'right', 'top', 'bottom'];
-export const size: Props['size'][] = ['XS', 'S', 'M', 'L', 'XL'];
+export const labelPositions: Props['labelPosition'][] = ['left', 'right', 'top', 'bottom'];
+export const sizes: Props['size'][] = ['XS', 'S', 'M', 'L', 'XL'];
 
 export const ThemeInterface: React.FC<ToggleTheme> = () => null;
 ThemeInterface.defaultProps = {
