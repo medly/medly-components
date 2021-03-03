e1b4ea75e34de5ae88e68d63071f3159795acd7a
gmukul01 <mukulbansal01@gmail.com>
2021-03-03 18:55:04 +0530

    fix: navigator undefined issue
    affects: @medly-components/utils

--- packages/utils/src/helpers/isMobile.ts
+++ packages/utils/src/helpers/isMobile.ts
@@ -1,1 +1,4 @@
-export const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+export const isMobile =
+    typeof window !== 'undefined'
+        ? /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent)
+        : false;
