a6097ee0f85f7941aac2ef30dc749260fcd77989
Pradip Bhusnar <47270141+Bhusnar5044@users.noreply.github.com>
2021-03-22 21:07:17 +0530

    Update types.ts
    Added new prop disabledBorderColor

--- packages/theme/src/core/input/types.ts
+++ packages/theme/src/core/input/types.ts
@@ -2,6 +2,7 @@ export interface InputTheme {
     color: string;
     bgColor: string;
     borderColor: string;
+    disabledBorderColor:string;
     descriptionColor: string;
     disabledBgcolor: string;
     focusedBorderColor: string;
