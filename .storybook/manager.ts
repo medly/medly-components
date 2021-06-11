611adf3b96dcbcde8a2aab60cc5aeb8229dc7755
gmukul01 <mukulbansal01@gmail.com>
2021-06-11 12:08:47 +0530

    docs: add basic meta tags in storybook

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,11 +4,14 @@ import { create } from '@storybook/theming/create';
 const theme = create({
     base: 'light',
     brandTitle: 'Medly-components',
+    brandUrl: 'https://medlycomponents.com',
     fontCode: 'monospace',
     fontBase: '"Open Sans", sans-serif'
 });
 
 addons.setConfig({
+    showNav: true,
+    showPanel: true,
     panelPosition: 'bottom',
     theme,
     previewTabs: {
