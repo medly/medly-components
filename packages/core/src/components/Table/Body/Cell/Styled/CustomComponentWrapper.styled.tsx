75420733cc49f6b0c4bb025ee4cb3d71cc9cad9c
dhirendra777 <dhirendra.naik@medlypharmacy.com>
2021-07-25 01:22:25 +0530

    fix(core): fix fitContent table cell width issue for custom cell component
    affects: @medly-components/core

--- packages/core/src/components/Table/Body/Cell/Styled/CustomComponentWrapper.styled.tsx
+++ packages/core/src/components/Table/Body/Cell/Styled/CustomComponentWrapper.styled.tsx
@@ -2,7 +2,7 @@ import { styled } from '@medly-components/utils';
 import { TableColumnConfig } from '../../../types';
 
 export const CustomComponentWrapper = styled('div')<{ align: TableColumnConfig['align'] }>`
-    width: 100%;
+    width: fit-content;
     display: flex;
     justify-content: ${({ align }) => (align === 'right' ? 'flex-end' : align === 'center' ? 'center' : 'flex-start')};
 `;
