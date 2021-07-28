e8e1d3041b6a89d08bd5d3ccde0ba1c617bda2f5
gmukul01 <mukulbansal01@gmail.com>
2021-07-28 19:23:01 +0530

    feat: add clear state function in useAxios hook
    affects: @medly-components/utils

--- packages/utils/src/hooks/useAxios/types.ts
+++ packages/utils/src/hooks/useAxios/types.ts
@@ -1,14 +1,15 @@
 import { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export type Result<Data = any, Error = any> = {
-    data: Data;
-    isLoading: boolean;
-    response: AxiosResponse<Data>;
-    error: AxiosResponse<Error>;
+    data: Data; //Data in the response
+    isLoading: boolean; //This variable will be set true when api call is in progress
+    response: AxiosResponse<Data>; //Success Response of the api call
+    error: AxiosResponse<Error>; //Error response of the api call
     request: (
         param: AxiosRequestConfig & {
             onSuccess?: (response?: AxiosResponse<Data>) => void; //Function to be called whenever there is successful response
             onError?: (error?: AxiosResponse<Error>) => void; //Function to be called whenever there is an error}
         }
     ) => Promise<{ response?: AxiosResponse<Data>; error?: AxiosResponse<Error> }>;
+    clear: () => void; //This can be used to clear all the state.
 };
