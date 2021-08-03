4ff6ab824fd3dd98859ade1699725c1efb05c6a1
Manish Ranjan <manish@Manishs-MacBook-Pro.local>
2021-08-03 17:38:43 +0530

    refactor: change table body props name to TableBodyProps
    affects: @medly-components/core

--- packages/core/src/components/Table/Body/types.ts
+++ packages/core/src/components/Table/Body/types.ts
@@ -1,4 +1,4 @@
-export interface BodyProps {
+export interface TableBodyProps {
     setUniqueIds: React.Dispatch<React.SetStateAction<any[]>>;
     selectedRowIds: Array<number | string>;
     onRowSelection: (id: number | string | Array<number | string>) => void;
