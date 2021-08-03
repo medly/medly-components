3e8e08e1b39af24797e71468853b2d6e5f5bea9e
Manish Ranjan <manish@Manishs-MacBook-Pro.local>
2021-08-03 17:47:03 +0530

    refactor: change commomprops name to LabelCommomProps
    affects: @medly-components/core

--- packages/core/src/components/Label/types.ts
+++ packages/core/src/components/Label/types.ts
@@ -3,7 +3,7 @@ import { HTMLProps, WithThemeProp } from '@medly-components/utils';
 
 export type LabelPositions = 'top' | 'right' | 'left' | 'bottom';
 
-export interface CommonProps extends HTMLProps<HTMLLabelElement>, WithThemeProp {
+export interface LabelCommonProps extends HTMLProps<HTMLLabelElement>, WithThemeProp {
     /** Shows pointer while hovering on it
      * @default false
      */
@@ -20,7 +20,7 @@ export interface CommonProps extends HTMLProps<HTMLLabelElement>, WithThemeProp 
     labelColor?: string;
 }
 
-export interface LabelProps extends CommonProps {
+export interface LabelProps extends LabelCommonProps {
     /** Label Variant */
     labelVariant?: FontVariants;
     /** Label Weight */
