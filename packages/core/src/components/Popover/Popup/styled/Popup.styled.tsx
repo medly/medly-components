5c2003ef1bcf5f2c5ccfc0212683e55262945443
Manish-Ranjan <46186440+Manish-Ranjan@users.noreply.github.com>
2021-08-03 04:05:25 -0700

    refactor: change popover prop name to PopoverProps (#355)
    affects: @medly-components/core
    
    Co-authored-by: Manish Ranjan <manish@Manishs-MacBook-Pro.local>
    Co-authored-by: Mukul Bansal <mukulbansal01@gmail.com>

--- packages/core/src/components/Popover/Popup/styled/Popup.styled.tsx
+++ packages/core/src/components/Popover/Popup/styled/Popup.styled.tsx
@@ -1,6 +1,6 @@
 import { css, fullHeight, fullWidth, styled } from '@medly-components/utils';
 import { Placement } from '../../types';
-import { PopupProps } from '../types';
+import { PopoverPopupProps } from '../types';
 import { getStyleWithArrow } from './WithArrow';
 
 export const getPosition = ({ placement, distance = '0px' }: { placement: Placement; distance?: string }) => {
@@ -72,7 +72,7 @@ export const getPosition = ({ placement, distance = '0px' }: { placement: Placem
     }
 };
 
-export const PopupStyled = styled('div')<PopupProps>`
+export const PopupStyled = styled('div')<PopoverPopupProps>`
     z-index: 4;
     position: absolute;
     background-color: ${({ bgColor }) => bgColor};
