67002ba28a3073f36f5a3886be10bafcf530db1a
Manish-Ranjan <46186440+Manish-Ranjan@users.noreply.github.com>
2021-08-03 04:24:11 -0700

    refactor: change searchbox prop name to SearchBoxProps (#358)
    affects: @medly-components/core
    
    Co-authored-by: Manish Ranjan <manish@Manishs-MacBook-Pro.local>
    Co-authored-by: Mukul Bansal <mukulbansal01@gmail.com>

--- packages/core/src/components/SearchBox/SearchBox.stories.tsx
+++ packages/core/src/components/SearchBox/SearchBox.stories.tsx
@@ -1,7 +1,7 @@
 import { defaultTheme, SearchBoxTheme } from '@medly-components/theme';
 import React from 'react';
 import Text from '../Text';
-import { Props } from './types';
+import { SearchBoxProps } from './types';
 
 export const optionsArray = [
     { value: 'Dummy1 option', label: 'Dummy1 option' },
@@ -14,7 +14,7 @@ export const optionsArray = [
     { value: <Text>Custom component</Text>, label: 'Custom component' }
 ];
 
-export const size: Props['size'][] = ['S', 'M'];
+export const size: SearchBoxProps['size'][] = ['S', 'M'];
 
 export const ThemeInterface: React.FC<SearchBoxTheme> = () => null;
 ThemeInterface.defaultProps = {
