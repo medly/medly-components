99900c4853d36fbdea1ac1378357cd3a5a3e6637
Manish Ranjan <manish@Manishs-MacBook-Pro.local>
2021-08-05 10:22:15 +0530

    feat: add a prop disabled in braedcrumb item
    affects: @medly-components/core, @medly-components/theme

--- packages/core/src/components/Breadcrumb/Item/Item.tsx
+++ packages/core/src/components/Breadcrumb/Item/Item.tsx
@@ -1,14 +1,16 @@
-import { HTMLProps, isValidStringOrNumber, WithStyle } from '@medly-components/utils';
+import { isValidStringOrNumber, WithStyle } from '@medly-components/utils';
 import React, { FC } from 'react';
 import Text from '../../Text';
 import { BreadcrumbItemStyled } from './Item.styled';
-
-export const BreadcrumbItem: FC<HTMLProps<HTMLLIElement>> & WithStyle = React.memo(
-    React.forwardRef(({ children, ...restProps }, ref) => (
-        <BreadcrumbItemStyled ref={ref} {...restProps}>
-            {isValidStringOrNumber(children) ? <Text>{children}</Text> : children}
-        </BreadcrumbItemStyled>
-    ))
+import { BreadcrumbItemProps } from './types';
+export const BreadcrumbItem: FC<BreadcrumbItemProps> & WithStyle = React.memo(
+    React.forwardRef(({ children, ...restProps }, ref) => {
+        return (
+            <BreadcrumbItemStyled ref={ref} {...restProps}>
+                {isValidStringOrNumber(children) ? <Text>{children}</Text> : children}
+            </BreadcrumbItemStyled>
+        );
+    })
 );
 
 BreadcrumbItem.displayName = 'BreadcrumbItem';
