3ad43f224f50ea95959b8b2db8cb3474cd853e98
Sameer Waskar <sameer.waskar@medly.com>
2021-08-05 14:33:54 +0530

    refactor(icons): renames caret-double icon to expand icon
    affects: @medly-components/icons

---
+++ packages/icons/src/icons/Action/ExpandIcon.tsx
@@ -0,0 +1,18 @@
+import { WithStyle } from '@medly-components/utils';
+import React, { FC } from 'react';
+import ExpandIconSvg from '../../assets/Action/expand_24px_rounded.svg';
+import SvgIcon, { SvgIconProps } from '../../SvgIcon';
+
+const ExpandIcon: FC<SvgIconProps> & WithStyle = React.memo(props => {
+    const { size, withHoverEffect, color, margin, ...restProps } = props;
+    return (
+        <SvgIcon {...{ size, withHoverEffect, color, margin, ...restProps }}>
+            <ExpandIconSvg {...restProps} width="1em" height="1em" />
+        </SvgIcon>
+    );
+});
+
+ExpandIcon.Style = SvgIcon;
+ExpandIcon.displayName = 'ExpandIcon';
+
+export default ExpandIcon
