4d956ff319e052c0b8fbed4f71728751c7ad0295
Sameer Waskar <sameer.waskar@medly.com>
2021-08-13 13:36:40 +0530

    feat(utils): updates the useKeyPress hook to support multiple keys press events
    affects: @medly-components/utils

--- packages/utils/src/hooks/useKeyPress/useKeyPress.ts
+++ packages/utils/src/hooks/useKeyPress/useKeyPress.ts
@@ -1,29 +1,40 @@
-import { useEffect, useState } from 'react';
 
-export const useKeyPress = (targetKey: string): boolean => {
-    const [keyPressed, setKeyPressed] = useState(false);
+import { useCallback, useEffect, useState } from 'react';
 
-    function downHandler({ key }: { key: string }) {
-        if (key === targetKey) {
-            setKeyPressed(true);
-        }
-    }
+export const useKeyPress = (targetKeys: Array<string>, defaultPrevented = false): boolean => {
+    const [keysPressed, setKeyPressed] = useState<Array<string>>([]),
+        [allKeysPressed, setAllKeysPressed] = useState<boolean>(false);
 
-    const upHandler = ({ key }: { key: string }) => {
-        if (key === targetKey) {
-            setKeyPressed(false);
-        }
-    };
+    const downHandler = useCallback(
+        (event: KeyboardEvent) => {
+            setKeyPressed(oldKeys => Array.from(new Set([...oldKeys, event.key])));
+            defaultPrevented && event.preventDefault();
+        },
+        [defaultPrevented]
+    );
+
+    const upHandler = useCallback(
+        (event: KeyboardEvent) => {
+            setKeyPressed(oldKeys => oldKeys.filter(oldKey => oldKey !== event.key));
+            defaultPrevented && event.preventDefault();
+        },
+        [defaultPrevented]
+    );
+
+    useEffect(() => {
+        setAllKeysPressed(
+            targetKeys.every((key, index) => keysPressed[index] === key)
+        );
+    }, [targetKeys, keysPressed]);
 
     useEffect(() => {
         window.addEventListener('keydown', downHandler);
         window.addEventListener('keyup', upHandler);
-
         return () => {
             window.removeEventListener('keydown', downHandler);
             window.removeEventListener('keyup', upHandler);
         };
-    });
+    }, []);
 
-    return keyPressed;
+    return allKeysPressed;
 };
