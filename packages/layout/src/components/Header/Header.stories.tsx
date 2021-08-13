07e2b2c789e070b168570779a6588f175a55f650
gmukul01 <mukulbansal01@gmail.com>
2021-08-13 14:22:35 +0530

    refactor: move demo header component to mdx file
    affects: @medly-components/layout

--- packages/layout/src/components/Header/Header.stories.tsx
+++ packages/layout/src/components/Header/Header.stories.tsx
@@ -1,10 +1,6 @@
-import Logo from '@medly-components/core/src/components/Logo';
-import { HomeIcon, LockIcon, PillIcon } from '@medly-components/icons';
 import { defaultTheme } from '@medly-components/theme';
-import { useMediaQuery } from '@medly-components/utils';
 import { HeaderTheme } from 'packages/theme/src/layout/header/types';
-import React, { FC } from 'react';
-import Header from './Header';
+import { FC } from 'react';
 import { Context } from './Header.context';
 import { HeaderStaticProps } from './types';
 
@@ -15,27 +11,3 @@ Theme.defaultProps = {
 
 export const StaticProps: FC<HeaderStaticProps> = () => null;
 export const ContextTheme: FC<Context> = () => null;
-
-export const FullSetup = () => {
-    const isNotMobile = useMediaQuery(`@media (min-width: ${defaultTheme.header.breakpoints.mobile})`);
-    return (
-        <Header>
-            <Logo showName={isNotMobile} />
-            <Header.Nav>
-                <Header.NavItem isActive>
-                    <HomeIcon size="S" iconColor={defaultTheme.colors.white} />
-                    My Dashboard
-                </Header.NavItem>
-                <Header.NavItem>
-                    <PillIcon size="S" iconColor={defaultTheme.colors.white} />
-                    Prescriptions
-                </Header.NavItem>
-                <Header.NavItem>
-                    <LockIcon size="S" iconColor={defaultTheme.colors.white} />
-                    Security
-                </Header.NavItem>
-            </Header.Nav>
-            <div>Logout</div>
-        </Header>
-    );
-};
