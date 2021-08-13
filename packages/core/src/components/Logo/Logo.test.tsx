2477249e14fa4b787b0de501a32c95764d5fea14
gmukul01 <mukulbansal01@gmail.com>
2021-08-13 14:21:27 +0530

    refactor: change prop of Logo component in docs and test
    affects: @medly-components/core

--- packages/core/src/components/Logo/Logo.test.tsx
+++ packages/core/src/components/Logo/Logo.test.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import Logo from './Logo';
 
 describe('Logo', () => {
-    const DummyLogo = <span>My Logo</span>;
-    const DummyName = <span>My Name</span>;
+    const DummyLogo = () => <span>My Logo</span>,
+        DummyName = () => <span>My Name</span>;
 
     const subject = (props = {}) => render(<Logo {...props} />);
 
     it('should load passed the logo and name', () => {
-        subject({ logo: DummyLogo, name: DummyName });
+        subject({ logo: <DummyLogo />, name: <DummyName /> });
 
         expect(screen.getByText('My Logo')).toBeInTheDocument();
         expect(screen.getByText('My Name')).toBeInTheDocument();
