94e168d8f6fab3d35582984ab88880714e4986bf
Sameer Waskar <sameer.waskar@medly.com>
2021-08-16 11:39:39 +0530

    feat(utils): adds examples for showing in storybook of useKeyPress with style
    affects: @medly-components/utils

---
+++ packages/utils/src/hooks/useKeyPress/useKeyPress.stories.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { Text } from '@medly-components/core';
+import { useKeyPress } from '@medly-components/utils';
+import { Div, DisplayDiv } from './useKeyPress.styled';
+
+export const Basic = () => {
+    const [counter, setCounter] = useState<number>(0);
+    const arrowUpPressed = useKeyPress('ArrowUp');
+    const arrowDownPressed = useKeyPress(['ArrowDown']);
+
+    useEffect(() => arrowUpPressed && setCounter(s => s + 1), [arrowUpPressed]);
+
+    useEffect(() => arrowDownPressed && setCounter(s => s - 1), [arrowDownPressed]);
+
+    return (
+        <>
+            <Text>Try pressing the following: </Text>
+            <Div>
+                <Text>
+                    1. Press <code>ArrowUp</code> ⬆️ key to increase the number.
+                </Text>
+            </Div>
+            <Div>
+                <Text>
+                    2. Press <code>ArrowDown</code> ⬇️ key to decrease the number.
+                </Text>
+            </Div>
+            <Div>
+                counter: <Text data-testid="counter" textWeight="Strong">{counter}</Text>
+            </Div>
+        </>
+    );
+};
+
+export const Compound = () => {
+    const isShiftCPressed = useKeyPress(['Shift', 'C']);
+    const isDhjPressed = useKeyPress(['d', 'h', 'j'], true);
+    return (
+        <>
+            <Text>Try pressing the following keys:</Text>
+            <Div>
+                1. <Text textWeight={isShiftCPressed ? 'Strong' : 'Regular'}>Shift + C</Text>
+            </Div>
+            <Div>
+                2. <Text textWeight={isDhjPressed ? 'Strong' : 'Regular'}>D + H + J</Text>
+            </Div>
+            <DisplayDiv>
+                {isShiftCPressed && '😊'}
+                {isDhjPressed && '👌🏻'}
+            </DisplayDiv>
+        </>
+    );
+};
