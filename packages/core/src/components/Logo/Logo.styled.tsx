0422adc582cb2d449a8fdf77186fb6e4ac6ed9a8
gmukul01 <mukulbansal01@gmail.com>
2021-08-18 16:02:24 +0530

    refactor: change div tag to button tag in logo component
    affects: @medly-components/core, @medly-components/layout

--- packages/core/src/components/Logo/Logo.styled.tsx
+++ packages/core/src/components/Logo/Logo.styled.tsx
@@ -1,10 +1,13 @@
 import { styled } from '@medly-components/utils';
 
-export const Container = styled.div`
+export const Container = styled.button`
     display: inline-grid;
     grid-auto-flow: column;
     grid-column-gap: 0.8rem;
     align-items: center;
     user-select: none;
+    padding: 0;
+    border: unset;
+    background: transparent;
     cursor: ${({ onClick }) => onClick && 'pointer'};
 `;
