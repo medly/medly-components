768268efbed574f94020877d36ec6df7fa99c4e2
Sameer Waskar <sameer.waskar@medly.com>
2021-08-18 15:34:47 +0530

    perf(utils): returns function if same key is pressed more than one time
    affects: @medly-components/utils

--- packages/utils/src/hooks/useKeyPress/useKeyPress.ts
+++ packages/utils/src/hooks/useKeyPress/useKeyPress.ts
@@ -1,11 +1,24 @@
 import { useCallback, useEffect, useState } from 'react';
 
+/**
+ * A custom hook to detect when the user is pressing a specific key or a collection of keys
+ *
+ * @param {targetKeys} -  The key(s) to watch
+ * @param {defaultPrevented} - To prevent the default action that belongs to the event
+ *
+ * @returns {boolean} - TRUE returns a match.
+ */
 export const useKeyPress = (targetKeys: string | string[], defaultPrevented = false): boolean => {
+    if (targetKeys.length === 0) {
+        throw new Error(`[Invalid parameter]: 'targetKeys' cannot be empty.`);
+    }
+
     const [keysPressed, setKeyPressed] = useState<string[]>([]),
         [allKeysPressed, setAllKeysPressed] = useState<boolean>(false);
 
     const downHandler = useCallback(
         (event: KeyboardEvent) => {
+            if (event.repeat) return;
             setKeyPressed(oldKeys => Array.from(new Set([...oldKeys, event.key])));
             defaultPrevented && event.preventDefault();
         },
