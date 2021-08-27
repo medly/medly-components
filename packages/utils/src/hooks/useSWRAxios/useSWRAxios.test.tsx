327a76b20f16f41d289a76b64a4587a3589e1344
gmukul01 <mukulbansal01@gmail.com>
2021-08-27 08:12:06 +0530

    test: update tests for useSWRAxios
    affects: @medly-components/utils

--- packages/utils/src/hooks/useSWRAxios/useSWRAxios.test.tsx
+++ packages/utils/src/hooks/useSWRAxios/useSWRAxios.test.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import { cache, SWRConfiguration } from 'swr';
 import { useSWRAxios } from './useSWRAxios';
 
-const DummyComp = ({ initialData }: SWRConfiguration) => {
-    const { data, error } = useSWRAxios<unknown, { message: string }>({ url: '/api/applications' }, { initialData });
+const DummyComp = ({ initialData, passUrlOnly = false }: SWRConfiguration & { passUrlOnly?: boolean }) => {
+    const { data, error } = useSWRAxios<unknown, { message: string }>(passUrlOnly ? '/api/applications' : { url: '/api/applications' }, {
+        initialData
+    });
     return (
         <>
             <p>{data}</p>
@@ -19,9 +21,16 @@ describe('useSWRAxios', () => {
         mockAxios.reset();
     });
 
-    it('should return expected data', async () => {
+    it('should return expected data is we pass axios config as first param', async () => {
         mockAxios.onGet('/api/applications').replyOnce(200, 'Hello');
         const { findByText } = renderWithSWR(<DummyComp />);
+        const message = await findByText('Hello');
+        expect(message).toBeInTheDocument();
+    });
+
+    it('should return expected data is we pass url as first param', async () => {
+        mockAxios.onGet('/api/applications').replyOnce(200, 'Hello');
+        const { findByText } = renderWithSWR(<DummyComp passUrlOnly />);
         const message = await findByText('Hello');
         expect(message).toBeInTheDocument();
     });
