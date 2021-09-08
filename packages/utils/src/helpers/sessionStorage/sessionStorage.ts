7060f16aa7625f68b4445b858a37bd8897e40695
Abhishek Kashyap <abhishek.kashyap@medlypharmacy.com>
2021-09-08 14:49:41 +0530

    fix: update localstorage property to session storage for session storage
    affects: @medly-components/utils

--- packages/utils/src/helpers/sessionStorage/sessionStorage.ts
+++ packages/utils/src/helpers/sessionStorage/sessionStorage.ts
@@ -1,4 +1,6 @@
 import STORAGE_UTILITIES from '../storage';
 import { StorageUtilities } from '../storage/types';
 
-export const SESSION_STORAGE: StorageUtilities = STORAGE_UTILITIES(typeof window === 'undefined' ? ({} as Storage) : window?.localStorage);
+export const SESSION_STORAGE: StorageUtilities = STORAGE_UTILITIES(
+    typeof window === 'undefined' ? ({} as Storage) : window?.sessionStorage
+);
