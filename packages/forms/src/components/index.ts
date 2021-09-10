52d3684d4cefbb8a55f0c0d5d38c927999efe307
gmukul01 <mukulbansal01@gmail.com>
2021-09-10 21:25:25 +0530

    fix: types of custom component in forms package
    affects: @medly-components/forms

--- packages/forms/src/components/index.ts
+++ packages/forms/src/components/index.ts
@@ -1,4 +1,5 @@
 import { FormActionSchema } from './Actions/types';
+import { FormCustomComponent, FormFieldProperties } from './Fields/types';
 import { FormFieldSchema } from './Form/types';
 export { default as Form } from './Form';
-export { FormFieldSchema, FormActionSchema };
+export { FormFieldSchema, FormActionSchema, FormCustomComponent, FormFieldProperties };
