f2a7fbeb36a13a7d8b46d39c09508eb98b3cda54
Aloralyn Ayran <30561199+aloralyn@users.noreply.github.com>
2021-09-14 10:16:45 -0400

    style(core): update helper tooltip used in textfield & add stories in docs
    affects: @medly-components/core

--- packages/core/src/components/TextField/Styled/HelperAndErrorTextTooltipWrapper.styled.tsx
+++ packages/core/src/components/TextField/Styled/HelperAndErrorTextTooltipWrapper.styled.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
-export const HelperAndErrorTextTooltipWrapper = styled.div<{ displayCharacterCount: boolean; size: 'S' | 'M' }>`
-    ${({ displayCharacterCount, size }) => (!displayCharacterCount || size !== 'S') && 'margin-left: 1rem;'}
+export const HelperAndErrorTextTooltipWrapper = styled.div<{ displayCharacterCount: boolean; size: 'S' | 'M'; isSuffixPresent: boolean}>`
+    margin-top: 0.5rem;
+    ${({ isSuffixPresent }) => !isSuffixPresent && 'margin-right: 1rem;'}
+    ${({ displayCharacterCount, size }) => (!displayCharacterCount || size !== 'S' ) && 'margin-left: 1rem;'}
 `;
