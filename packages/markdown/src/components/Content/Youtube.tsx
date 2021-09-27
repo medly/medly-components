18ddcf97b3e46b9dbe384d4460648ba062296c5a
Abhishek Kashyap <abhishek.kashyap@medlypharmacy.com>
2021-09-27 17:05:02 +0530

    feat(markdown): add youtube component
    affects: @medly-components/markdown

---
+++ packages/markdown/src/components/Content/Youtube.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/display-name */
+import React from 'react';
+import { CodeComponent } from 'react-markdown/lib/ast-to-react';
+
+export const YouTube: CodeComponent = React.memo(props => {
+    // @ts-ignore
+    const params = new URL(props.link || props.children[0]?.props?.href).searchParams;
+    return (
+        <iframe
+            src={`https://www.youtube.com/embed/${params.get('v') || props.id}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+        />
+    );
+});
