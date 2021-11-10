44a05513bd0aed2195ee0f4a340e42d5af8b69b6
gmukul01 <mukulbansal01@gmail.com>
2021-11-10 10:48:19 +0530

    feat: use new JSX transform

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,75 @@
 module.exports = function (api) {
-    const ignore = [],
-        overrides = [],
-        plugins = ['polished'];
-
-    if (api.env('production')) {
-        ignore.push('**/*.test.tsx', '**/*.test.ts', '**/test-utils.tsx', '**/*.stories.mdx', '**/*.stories.tsx', '__snapshots__', 'docs');
-        overrides.push({
-            plugins: [['react-remove-properties', { properties: [] }]]
-        });
-    }
+    const overrides = [],
+        ignore = ['node_modules'],
+        plugins = [
+            'polished',
+            '@babel/plugin-transform-runtime',
+            [
+                'babel-plugin-styled-components',
+                {
+                    pure: true,
+                    minify: true,
+                    fileName: api.env('storybook'),
+                    displayName: api.env('storybook'),
+                    transpileTemplateLiterals: true
+                }
+            ],
+            [
+                'inline-react-svg',
+                {
+                    svgo: {
+                        plugins: [
+                            {
+                                cleanupIDs: {
+                                    minify: false
+                                }
+                            }
+                        ]
+                    }
+                }
+            ],
+            [
+                '@babel/plugin-transform-react-jsx',
+                {
+                    runtime: api.env('production') ? 'automatic' : 'classic'
+                }
+            ]
+        ],
+        presets = [
+            [
+                '@babel/env',
+                {
+                    modules: api.env('production') ? false : 'auto'
+                }
+            ],
+            [
+                '@babel/preset-react',
+                {
+                    runtime: api.env('production') ? 'automatic' : 'classic'
+                }
+            ],
+            '@babel/typescript'
+        ];
 
     if (api.env('development') || api.env('storybook')) {
         ignore.push('**/*.test.tsx', '**/*.test.ts', '__snapshots__');
         plugins.push(['@babel/plugin-transform-modules-commonjs']);
+        overrides.push({
+            plugins: [
+                [
+                    'babel-plugin-styled-components',
+                    {
+                        fileName: true,
+                        displayName: true
+                    }
+                ]
+            ]
+        });
     }
 
-    return { extends: '@medly/babel-config-react', plugins, ignore, overrides };
+    if (api.env('production')) {
+        ignore.push('**/*.test.tsx', '**/*.test.ts', '**/test-utils.tsx', '**/*.stories.mdx', '**/*.stories.tsx', '__snapshots__', 'docs');
+    }
+
+    return { plugins, ignore, presets, overrides };
 };
