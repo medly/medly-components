0e124a627148fb19b8e026332d55e2490cd80c0a
Abhishek Kashyap <abhishek.kashyap@medly.com>
2021-11-15 22:31:10 +0530

    refactor: remove react imports from icons package (10/15)
    affects: @medly-components/icons

--- packages/icons/src/icons/GSDD/Warning53LineIcon.tsx
+++ packages/icons/src/icons/GSDD/Warning53LineIcon.tsx
@@ -1,9 +1,9 @@
 import { WithStyle } from '@medly-components/utils';
-import React, { FC } from 'react';
+import { memo, FC } from 'react';
 import Warning53LineIconSvg from '../../assets/GSDD/Warning_53_Line.svg';
 import SvgIcon, { SvgIconProps } from '../../SvgIcon';
 
-const Component: FC<SvgIconProps> = React.memo(props => {
+const Component: FC<SvgIconProps> = memo(props => {
     const { size, withHoverEffect, color, margin, ...restProps } = props;
     return (
         <SvgIcon {...{ size, withHoverEffect, color, margin, ...restProps }}>
