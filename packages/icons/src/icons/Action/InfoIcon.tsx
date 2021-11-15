eedc9dce09f9b147494dfb8d2578b2217aa4476d
Abhishek Kashyap <abhishek.kashyap@medly.com>
2021-11-15 21:01:52 +0530

    refactor: remove react imports from icons package (2/15)
    affects: @medly-components/icons

--- packages/icons/src/icons/Action/InfoIcon.tsx
+++ packages/icons/src/icons/Action/InfoIcon.tsx
@@ -1,9 +1,9 @@
 import { WithStyle } from '@medly-components/utils';
-import React, { FC } from 'react';
+import { memo, FC } from 'react';
 import InfoIconSvg from '../../assets/Action/info_24px_rounded.svg';
 import SvgIcon, { SvgIconProps } from '../../SvgIcon';
 
-const Component: FC<SvgIconProps> = React.memo(props => {
+const Component: FC<SvgIconProps> = memo(props => {
     const { size, withHoverEffect, color, margin, ...restProps } = props;
     return (
         <SvgIcon {...{ size, withHoverEffect, color, margin, ...restProps }}>
