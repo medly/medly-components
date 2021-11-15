8ec19191450cd71ed794181b3b4d38f471e946da
Abhishek Kashyap <abhishek.kashyap@medly.com>
2021-11-15 22:35:10 +0530

    refactor: remove react imports from icons package (11/15)
    affects: @medly-components/icons

--- packages/icons/src/icons/Image/CameraAltIcon.tsx
+++ packages/icons/src/icons/Image/CameraAltIcon.tsx
@@ -1,9 +1,9 @@
 import { WithStyle } from '@medly-components/utils';
-import React, { FC } from 'react';
+import { memo, FC } from 'react';
 import CameraAltIconSvg from '../../assets/Image/camera_alt_24px_rounded.svg';
 import SvgIcon, { SvgIconProps } from '../../SvgIcon';
 
-const Component: FC<SvgIconProps> = React.memo(props => {
+const Component: FC<SvgIconProps> = memo(props => {
     const { size, withHoverEffect, color, margin, ...restProps } = props;
     return (
         <SvgIcon {...{ size, withHoverEffect, color, margin, ...restProps }}>
--- packages/icons/src/icons/Image/Crop75Icon.tsx
+++ packages/icons/src/icons/Image/Crop75Icon.tsx
@@ -1,9 +1,9 @@
 import { WithStyle } from '@medly-components/utils';
-import React, { FC } from 'react';
+import { memo, FC } from 'react';
 import Crop75IconSvg from '../../assets/Image/crop_7_5_24px_rounded.svg';
 import SvgIcon, { SvgIconProps } from '../../SvgIcon';
 
-const Component: FC<SvgIconProps> = React.memo(props => {
+const Component: FC<SvgIconProps> = memo(props => {
     const { size, withHoverEffect, color, margin, ...restProps } = props;
     return (
         <SvgIcon {...{ size, withHoverEffect, color, margin, ...restProps }}>
