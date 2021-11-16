8fa5715a83ed278367e65ae6794de81a26566414
Mukul Bansal <mukulbansal01@gmail.com>
2021-11-16 12:43:13 +0530

    refactor: updateNestedValue function to accept any type (#524)
    affects: @medly-components/utils

--- packages/utils/src/helpers/updateNestedValue/updateNestedValue.ts
+++ packages/utils/src/helpers/updateNestedValue/updateNestedValue.ts
@@ -1,7 +1,7 @@
-export const updateNestedValue = (obj: { [k: string]: any }, key: string, value: any): Record<string, unknown> => {
+export const updateNestedValue = <T extends Record<string, any>>(obj: T, key: string, value: any): T => {
     const keys = key.split(/\.(.+)/);
     if (keys.length === 1) {
-        return { ...obj, [key]: value };
+        return { ...obj, [key]: value } as T;
     } else if (keys.length > 1) {
         return {
             ...obj,
@@ -9,7 +9,7 @@ export const updateNestedValue = (obj: { [k: string]: any }, key: string, value:
                 ...obj[keys[0]],
                 ...updateNestedValue(obj[keys[0]], keys[1], value)
             }
-        };
+        } as T;
     } else {
         return value;
     }
