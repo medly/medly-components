2157d14249d45383c6d5f81b643f5cd8c273f6b9
Abhishek Kashyap <abhishek.kashyap@medly.com>
2021-11-16 12:43:31 +0530

    refactor: remove react imports from forms package (#520)
    affects: @medly-components/forms

--- packages/forms/src/components/Actions/Actions.tsx
+++ packages/forms/src/components/Actions/Actions.tsx
@@ -1,9 +1,9 @@
 import { Button } from '@medly-components/core';
-import React from 'react';
+import { FC, memo } from 'react';
 import * as Styled from './Actions.styled';
 import { ActionProps } from './types';
 
-export const Actions: React.FC<ActionProps> = React.memo(({ formId, disabled, actionLabel, actionSchema, isLoading }) => {
+export const Actions: FC<ActionProps> = memo(({ formId, disabled, actionLabel, actionSchema, isLoading }) => {
     return (
         <Styled.Actions alignItems={actionSchema?.alignItems} flexDirection={actionSchema?.flexDirection || 'row'}>
             {actionSchema ? (
