2fddddc7280f1b5886a762d9df51196200443018
gmukul01 <mukulbansal01@gmail.com>
2021-11-30 17:09:09 +0530

    style: update border radius of modal and dialog box
    affects: @medly-components/core, @medly-components/theme

--- packages/theme/src/core/modal/index.ts
+++ packages/theme/src/core/modal/index.ts
@@ -7,7 +7,7 @@ const modal: ModalTheme = {
     headerColor: colors.black,
     backgroundColor: colors.white,
     scrollbarThumbColor: colors.grey[400],
-    borderRadius: '2.4rem',
+    borderRadius: '1.2rem',
     closeIcon: {
         borderRadius: '50%',
         border: 'none',
