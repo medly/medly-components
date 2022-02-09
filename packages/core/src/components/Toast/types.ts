70384c611510005d5e0c9a28406a1fc406930177
Mukul Bansal <mukulbansal01@gmail.com>
2022-02-09 14:37:48 +0530

    feat: add functionality to add custom header and message component in… (#585)
    * feat: add functionality to add custom header and message component in toast component
    
    affects: @medly-components/core
    
    * fix: failing snapshots
    
    affects: @medly-components/core

--- packages/core/src/components/Toast/types.ts
+++ packages/core/src/components/Toast/types.ts
@@ -1,6 +1,6 @@
 import { HTMLProps, Omit } from '@medly-components/utils';
-import { ButtonProps } from '../Button/types';
 import type { FC } from 'react';
+import { ButtonProps } from '../Button/types';
 
 export type ToastVariants = 'success' | 'error' | 'info' | 'warning';
 
@@ -12,13 +12,15 @@ export type ToastProps = {
     /** Icon to be shown in the toast */
     icon?: FC<any>;
     /** Toast Header */
-    header?: string;
+    header?: string | React.ReactElement<any>;
     /** Toast message */
-    message?: string;
+    message?: string | React.ReactElement<any>;
     /** Additional action if you want to add */
     action?: { label: string } & Omit<ButtonProps, 'variant' | 'size'>;
     /** Set it true if you are using it without toast container and want to make it full width */
     fullWidth?: boolean;
+    /** Hide close icon */
+    hideCloseIcon?: boolean;
 };
 
 export type ToastComponentProps = Omit<HTMLProps<HTMLDivElement>, 'id' | 'action'> & ToastProps;
