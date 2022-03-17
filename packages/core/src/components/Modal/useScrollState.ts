404d8e01249711dfbc2d69fa54040d3e9982bf40
Nicolae Balica <nicolae.balica@medly.com>
2022-03-17 19:22:24 +0200

    fix: modal shadow is persistently showing. PMMPP-574

--- packages/core/src/components/Modal/useScrollState.ts
+++ packages/core/src/components/Modal/useScrollState.ts
@@ -5,14 +5,15 @@ import type { UIEvent } from 'react';
 export const useScrollState = ({ ref, scrollState, dispatch }: UseScrollStateProps): ((e: React.UIEvent<HTMLDivElement>) => void) => {
     useEffect(() => {
         const element = ref.current as HTMLElement;
-        ref.current && dispatch({ type: 'scrolledToBottom', value: element.getBoundingClientRect().height === element.scrollHeight });
+        ref.current &&
+            dispatch({ type: 'scrolledToBottom', value: Math.floor(element.getBoundingClientRect().height) === element.scrollHeight });
     }, [ref.current, ref.current?.scrollHeight]);
 
     const handleScroll = (e: UIEvent<HTMLDivElement>) => {
         // e.stopPropagation(); /* Allowing event to propagate for table minimap table scroll handle */
         const element = e.target as HTMLElement,
             isScrolledToTop = element.scrollTop === 0,
-            isScrolledToBottom = Math.ceil(element.scrollTop + element.getBoundingClientRect().height) === element.scrollHeight;
+            isScrolledToBottom = Math.floor(element.scrollTop + element.getBoundingClientRect().height) === element.scrollHeight;
 
         scrollState.scrolledToTop !== isScrolledToTop && dispatch({ type: 'scrolledToTop', value: isScrolledToTop });
         scrollState.scrolledToBottom !== isScrolledToBottom && dispatch({ type: 'scrolledToBottom', value: isScrolledToBottom });
