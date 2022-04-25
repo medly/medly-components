eb59713dd3c31e83cccc329086835a1ad8a6b24c
gmukul01 <mukulbansal01@gmail.com>
2022-04-25 10:41:29 +0530

    feat: add masking on copy paste in textfield component
    affects: @medly-components/core

--- packages/core/src/components/TextField/getMaskedValue.ts
+++ packages/core/src/components/TextField/getMaskedValue.ts
@@ -1,29 +1,23 @@
-import type { ChangeEvent } from 'react';
-
-const getMaskedValue = (event: ChangeEvent<HTMLInputElement>, mask: string): string => {
-    const { value } = event.target,
+const applyMasking = (value: string, mask: string): string => {
+    const { length } = value,
+        lastChar = value[length - 1],
         alphaNumericRegex = /[a-zA-Z0-9]/,
-        specialCharsRegex = /[^a-zA-Z0-9]/,
-        { length } = value,
-        lastChar = value[length - 1];
+        specialCharsRegex = /[^a-zA-Z0-9]/;
 
     let newValue;
 
-    // if user deletes some char from middle
-    if (event.target.selectionStart && event.target.selectionStart < length) {
-        newValue = value.slice(0, event.target.selectionStart);
-    } else if (length > mask.length || /[a-zA-Z]/.test(lastChar)) {
+    if (length > mask.length || /[a-zA-Z]/.test(lastChar)) {
         // if user types more char then mask length
         newValue = value.slice(0, -1);
-    } else if (specialCharsRegex.test(value[length - 1])) {
-        // if user deletes till the special character
-        newValue = value.replace(/[^a-zA-Z0-9]+$/, ''); //NOSONAR
     } else if (specialCharsRegex.test(mask[length - 1])) {
         // if user reaches to special character
         const remainingMask = mask.substr(length - 1),
             numberIndex = remainingMask.match(alphaNumericRegex)?.index;
 
         newValue = value.slice(0, -1) + remainingMask.substr(0, numberIndex) + value.slice(-1);
+    } else if (specialCharsRegex.test(mask[length])) {
+        // if user deletes the last special character
+        newValue = value;
     } else {
         const remainingMask = mask.substr(length),
             numberIndex = remainingMask.match(alphaNumericRegex)?.index;
@@ -32,6 +26,46 @@ const getMaskedValue = (event: ChangeEvent<HTMLInputElement>, mask: string): str
     }
 
     return newValue;
+};
+
+export const getMaskedValue = (event: React.ChangeEvent<HTMLInputElement>, mask: string) => {
+    const { value, selectionStart } = event.target;
+    let maskedValue;
+
+    if (selectionStart && selectionStart < value.length) {
+        maskedValue = value.slice(0, selectionStart);
+    } else {
+        maskedValue = value
+            .replace(/[^a-zA-Z0-9]/g, '') //NOSONAR
+            .split('')
+            .reduce((acc: string, c: string) => applyMasking(acc + c, mask), '');
+    }
+
+    //TODO: Handle the case when the user deletes the character in the middle of the mask
+    // const cursorPosition = selectionStart || 0;
+
+    // event.target.value = maskedValue;
+
+    // value.match(/[^a-zA-Z0-9]/) &&
+    //     cursorPosition > 0 &&
+    //     cursorPosition < maskedValue.length &&
+    //     !/[^a-zA-Z0-9]/.test(maskedValue[cursorPosition - 1]) &&
+    //     event?.target?.setSelectionRange(cursorPosition, cursorPosition);
+
+    // if (
+    //     value.match(/[^a-zA-Z0-9]/) &&
+    //     cursorPosition > 0 &&
+    //     cursorPosition < maskedValue.length &&
+    //     /[^a-zA-Z0-9]/.test(maskedValue[cursorPosition - 1])
+    // ) {
+    //     const indexOf = maskedValue.substr(cursorPosition - 1).match(/[a-zA-Z0-9]/);
+    //     if ((indexOf?.index || 0) > 0) {
+    //         const position = (indexOf?.input || '').length === (indexOf?.index || 0) + 1 ? indexOf?.index || 0 : (indexOf?.index || 0) - 1;
+    //         event?.target?.setSelectionRange(cursorPosition + position, cursorPosition + position);
+    //     }
+    // }
+
+    return maskedValue;
 };
 
 export default getMaskedValue;
