3f6b8e1431bc56ddb58fcf63a57534b9d906e151
Abhishek Kashyap <abhishek.kashyap@medly.com>
2022-06-15 14:18:00 +0530

    feat: add support for keybinding to select row in Table component (#673)
    * feat: add provision for select row keybinding
    
    affects: @medly-components/core
    
    * feat: add keybinding for selectRow
    
    affects: @medly-components/core
    
    * fix: rename rowClick -> clickRow for similar semantics
    
    affects: @medly-components/core
    
    * feat: add logic for rowSelection on keydown
    
    affects: @medly-components/core
    
    * test: add test for row selection on custom keypress
    
    affects: @medly-components/core
    
    * docs: update docs to notify about custom keybind for row selection
    
    affects: @medly-components/core
    
    * chore: remove unnecessary console.log
    
    affects: @medly-components/core

--- packages/core/src/components/Table/constants.ts
+++ packages/core/src/components/Table/constants.ts
@@ -13,5 +13,6 @@ export const defaultKeyBindings: KeyBindings = {
     down: 'ArrowDown',
     expandRow: 'ArrowRight',
     collapseRow: 'ArrowLeft',
-    rowClick: 'Enter'
+    clickRow: 'Enter',
+    selectRow: ' '
 };
