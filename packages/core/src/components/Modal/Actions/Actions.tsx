c0e77446760bc6ec8f081361077608649676d26f
Mukul Bansal <mukulbansal01@gmail.com>
2023-02-09 01:30:23 +0000

    feat: pass restProps to modal actions component
    affects: @medly-components/core

--- packages/core/src/components/Modal/Actions/Actions.tsx
+++ packages/core/src/components/Modal/Actions/Actions.tsx
@@ -1,14 +1,18 @@
 import { WithStyle } from '@medly-components/utils';
-import { useContext, memo } from 'react';
+import type { FC } from 'react';
+import { memo, useContext } from 'react';
 import { ModalContext } from '../Modal.context';
 import * as Styled from './Actions.styled';
 import { ModalActionUserProps } from './types';
-import type { FC } from 'react';
 
-const Component: FC<ModalActionUserProps> = memo(({ children, alignItems }) => {
+const Component: FC<ModalActionUserProps> = memo(({ children, alignItems, ...restProps }) => {
     const { id, scrollState, isSmallScreen } = useContext(ModalContext);
 
-    return <Styled.Actions {...{ scrollState, isSmallScreen, alignItems, id: `${id}-actions` }}>{children}</Styled.Actions>;
+    return (
+        <Styled.Actions {...{ scrollState, isSmallScreen, alignItems, id: `${id}-actions` }} {...restProps}>
+            {children}
+        </Styled.Actions>
+    );
 });
 Component.displayName = 'Actions';
 export const Actions: FC<ModalActionUserProps> & WithStyle = Object.assign(Component, { Style: Styled.Actions });
