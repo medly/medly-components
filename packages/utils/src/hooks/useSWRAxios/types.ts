8b30a10215aed7821edffb99e5b1ce66cd4507e3
Mukul Bansal <mukulbansal01@gmail.com>
2023-04-02 02:47:49 +0100

    feat: update useSWRAxios return type
    affects: @medly-components/utils

--- packages/utils/src/hooks/useSWRAxios/types.ts
+++ packages/utils/src/hooks/useSWRAxios/types.ts
@@ -3,10 +3,25 @@ import { SWRConfiguration, SWRResponse } from 'swr';
 
 export type AxiosConfig = AxiosRequestConfig & { url: string };
 
-export type Return<Data = unknown, Error = unknown> = Omit<SWRResponse<AxiosResponse<Data>, AxiosError<Error>>, 'data' | 'error'> & {
-    data?: Data;
-    response?: AxiosResponse<Data>;
-    error?: AxiosResponse<Error>;
+type BlockingData<Data = any, Options = SWRConfiguration<Data>> = Options extends undefined
+    ? false
+    : Options extends {
+          suspense: true;
+      }
+    ? true
+    : Options extends {
+          fallbackData: Data;
+      }
+    ? true
+    : false;
+
+export type Return<Data = unknown, Error = unknown, Config = SWRConfiguration<Data, Error>> = Omit<
+    SWRResponse<AxiosResponse<Data>, AxiosError<Error>>,
+    'data' | 'error'
+> & {
+    data: BlockingData<Data, Config> extends true ? Data : Data | undefined;
+    response?: AxiosResponse<Data> | AxiosResponse<Error>;
+    error?: Error;
 };
 
 export type SWRConfig<Data = unknown, Error = unknown> = Omit<SWRConfiguration<AxiosResponse<Data>, AxiosError<Error>>, 'fallbackData'> & {
