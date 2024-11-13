b6102b1c62897777bb9383ff8ef4cadb9e73f484
Mukul Bansal <mukulbansal01@gmail.com>
2024-11-13 12:55:34 +0530

    fix: remove border of expanded row when we hide row action cell
    affects: @medly-components/core

--- packages/core/src/components/Table/Body/Cell/ExtendedRowCell/ExtendedRowCell.styled.tsx
+++ packages/core/src/components/Table/Body/Cell/ExtendedRowCell/ExtendedRowCell.styled.tsx
@@ -16,7 +16,7 @@ export const ExtendedRowCellStyled = styled('td')<{ tableSize: TableProps['size'
         left: ${({ tableSize }) => (tableSize === 'L' ? '2.4rem' : '1.6rem')};
         position: absolute;
         width: calc(100% - ${({ tableSize }) => (tableSize === 'L' ? '4.8rem' : '3.2rem')});
-        border-top: 1px solid ${({ theme }) => theme.table.borderColor};
+        border-top: ${({ theme, hideRowActionsCell }) => (hideRowActionsCell ? '' : `1px solid ${theme.table.borderColor}`)};
     }
 `;
 const tableSizePaddingMap = {
