d413d39f566ad6f0d412e1d405eb182262322e3b
Mukul Bansal <mukulbansal01@gmail.com>
2025-01-26 16:36:15 +0530

    feat: autoscroll singleselect options in view
    affects: @medly-components/core

--- packages/core/src/components/SingleSelect/Options/Options.tsx
+++ packages/core/src/components/SingleSelect/Options/Options.tsx
@@ -1,26 +1,34 @@
-import { WithStyle } from '@medly-components/utils';
+import { useCombinedRefs, WithStyle } from '@medly-components/utils';
 import type { FC } from 'react';
-import { forwardRef, memo } from 'react';
+import { forwardRef, memo, useEffect, useRef } from 'react';
 import Option from './Option';
 import * as Styled from './Options.styled';
 import { OptionsProps } from './types';
 const Component: FC<OptionsProps> = memo(
-    forwardRef(({ options, onOptionClick, hasError, ...restProps }, ref) => (
-        <Styled.Options {...restProps} ref={ref}>
-            {options.map(option => (
-                <Option
-                    key={option.value}
-                    {...option}
-                    variant={restProps.variant}
-                    onClick={onOptionClick}
-                    hasError={hasError}
-                    size={restProps.size}
-                    maxWidth={restProps.maxWidth}
-                    includesNestedOptions={restProps.includesNestedOptions}
-                />
-            ))}
-        </Styled.Options>
-    ))
+    forwardRef(({ options, onOptionClick, hasError, ...restProps }, ref) => {
+        const optionsRef = useCombinedRefs<HTMLUListElement>(ref, useRef(null));
+
+        useEffect(() => {
+            optionsRef?.current?.scrollIntoView({ behavior: 'smooth' });
+        }, []);
+
+        return (
+            <Styled.Options {...restProps} ref={optionsRef}>
+                {options.map(option => (
+                    <Option
+                        key={option.value}
+                        {...option}
+                        variant={restProps.variant}
+                        onClick={onOptionClick}
+                        hasError={hasError}
+                        size={restProps.size}
+                        maxWidth={restProps.maxWidth}
+                        includesNestedOptions={restProps.includesNestedOptions}
+                    />
+                ))}
+            </Styled.Options>
+        );
+    })
 );
 Component.defaultProps = {
     isNested: false
